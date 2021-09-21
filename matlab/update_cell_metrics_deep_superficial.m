3d362b37fe8610f21f65eeeca9ace0f9a133f222
Ryan Harvey <ryanh412@gmail.com>
2021-09-21 13:48:39 -0400

    empty max wave exception

--- matlab/update_cell_metrics_deep_superficial.m
+++ matlab/update_cell_metrics_deep_superficial.m
@@ -6,13 +6,15 @@
 df = readtable('D:\projects\ripple_heterogeneity\swr_pipe_all.csv');
 basepaths = unique(df.basepath);
 
+overwrite = true;
+
 for i = 1:length(basepaths)
     basepath = basepaths{i};
     basename = basenameFromBasepath(basepath);
     load(fullfile(basepath,[basename,'.cell_metrics.cellinfo.mat']))
     disp(basepath)
     
-    if all(contains(cell_metrics.deepSuperficial,"Unknown"))
+    if all(contains(cell_metrics.deepSuperficial,"Unknown")) || overwrite
         
         deepSuperficial_file = fullfile(basepath,[basename,'.deepSuperficialfromRipple.channelinfo.mat']);
         
@@ -25,10 +27,15 @@ for i = 1:length(basepaths)
         cell_metrics.general.deepSuperficial_file = deepSuperficial_file;
         
         for j = 1:cell_metrics.general.cellCount
-            cell_metrics.deepSuperficial(j) =...
-                deepSuperficial_ChClass(spikes.maxWaveformCh1(j)); % cell_deep_superficial OK
-            cell_metrics.deepSuperficialDistance(j) =...
-                deepSuperficial_ChDistance(spikes.maxWaveformCh1(j)); % cell_deep_superficial_distance
+            if isnan(spikes.maxWaveformCh1(j))
+                cell_metrics.deepSuperficial(j) = {'Unknown'};
+                cell_metrics.deepSuperficialDistance(j) = NaN;
+            else
+                cell_metrics.deepSuperficial(j) =...
+                    deepSuperficial_ChClass(spikes.maxWaveformCh1(j)); % cell_deep_superficial OK
+                cell_metrics.deepSuperficialDistance(j) =...
+                    deepSuperficial_ChDistance(spikes.maxWaveformCh1(j)); % cell_deep_superficial_distance
+            end
         end
         save(fullfile(basepath,[basename,'.cell_metrics.cellinfo.mat']),'cell_metrics')
     end
