2252ddd9fdc07fb6d5ace769cec946ce85c6c0a7
Ryan Harvey <ryanh412@gmail.com>
2021-10-05 17:22:33 -0400

    changed csv

--- matlab/load_all_sessions.m
+++ matlab/load_all_sessions.m
@@ -1,5 +1,5 @@
 % load_all_sessions
-df = readtable('D:\projects\ripple_heterogeneity\swr_pipe_all.csv');
+df = readtable('D:\projects\ripple_heterogeneity\sessions.csv');
 basepaths = unique(df.basepath);
 for i = 1:length(basepaths)
    basenames{i} = basenameFromBasepath(basepaths{i});
