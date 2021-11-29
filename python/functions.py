50698e891c1953b27dda7f67645074fd14a815d1
Ryan Harvey <ryanh412@gmail.com>
2021-11-29 12:12:45 -0500

    added compress_repeated_epochs

--- python/functions.py
+++ python/functions.py
@@ -425,3 +425,49 @@ def BurstIndex_Royer_2012(autocorrs):
         else:
             burst_idx.append(np.nan)
     return burst_idx
+
+def compress_repeated_epochs(epoch_df):
+    """
+    compress_repeated_epochs: Compresses epoch_df loaded by loading.load_epoch()
+    If there are back to back epochs of the same name, it will combine them
+
+    Input: epoch_df (uses: loading.load_epoch(basepath))
+    Output: Compressed epoch_df
+
+    Ryan H
+    """
+    match = np.zeros([epoch_df.environment.shape[0]])
+    match[match == 0] = np.nan
+    for i,ep in enumerate(epoch_df.environment[:-1]):
+        if np.isnan(match[i]):
+            # find match in current and next epoch
+            if ep == epoch_df.environment.iloc[i+1]:
+                match[i:i+2] = i
+                # given match, see if there are more matches
+                for match_i in np.arange(1,epoch_df.environment[:-1].shape[0]):
+                    if i+1+match_i == epoch_df.environment.shape[0]:
+                        break
+                    if ep == epoch_df.environment.iloc[i+1+match_i]:
+
+                        match[i:i+1+match_i+1] = i
+                    else:
+                        break
+
+    for i in range(len(match)):
+        if np.isnan(match[i]):
+            match[i] = (i+1)*2000 # make nans large numbers that are unlikely to be real epoch
+
+    # iter through each epoch indicator to get start and stop
+    results = pd.DataFrame()
+    no_nan_match = match[~np.isnan(match)]
+    for m in pd.unique(no_nan_match):
+        temp_dict = {'name': epoch_df[match==m].name.iloc[0],
+                    'startTime':epoch_df[match==m].startTime.iloc[0],
+                    'stopTime':epoch_df[match==m].stopTime.iloc[-1],
+                    'environment':epoch_df[match==m].environment.iloc[0],
+                    'behavioralParadigm':epoch_df[match==m].behavioralParadigm.iloc[0],
+                    }
+        temp_df = pd.DataFrame.from_dict(temp_dict,orient='index').T
+
+        results = results.append(temp_df,ignore_index=True)
+    return results    
