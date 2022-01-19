201552ee01ed77f49043eae18f7d03ca966b6dab
Ryan Harvey <ryanh412@gmail.com>
2022-01-19 16:09:56 -0500

    a bit more work to fill out pipeline

--- matlab/run_dataset/place_cells.m
+++ matlab/run_dataset/place_cells.m
@@ -19,7 +19,14 @@ disp(basepath)
 basename = basenameFromBasepath(basepath);
 load(fullfile(basepath,[basename,'.animal.behavior.mat']))
 load(fullfile(basepath,[basename,'.spikes.cellinfo.mat']))
+load(fullfile(basepath,[basename,'.session.mat']))
 
+epoch_df = put_epochs_into_usable_format(session);
+    
+% remove sleep and wheel running
+epoch_df = epoch_df(~contains(epoch_df.environment , 'sleep') &...
+                    ~contains(epoch_df.environment , 'wheel'),:);
+    
 pos{1} = [behavior.time',...
     behavior.position.linearized'];
 
@@ -35,4 +42,20 @@ for i = 1:length(firingMaps.rateMaps)
     map.y = firingMaps.params.y;
     placeFieldStats{i} = MapStats(map);
 end
+end
+
+function epoch_df = put_epochs_into_usable_format(session)
+epoch_df = table();
+for ep = 1:length(session.epochs)
+    name{ep,1} = session.epochs{ep}.name;
+    startTime(ep,1) = session.epochs{ep}.startTime;
+    stopTime(ep,1) = session.epochs{ep}.stopTime;
+    behavioralParadigm(ep,1)= session.epochs{ep}.behavioralParadigm;
+    environment{ep,1} = session.epochs{ep}.environment;
+end
+epoch_df.name = name;
+epoch_df.startTime = startTime;
+epoch_df.stopTime = stopTime;
+epoch_df.behavioralParadigm = behavioralParadigm;
+epoch_df.environment = environment;
 end
