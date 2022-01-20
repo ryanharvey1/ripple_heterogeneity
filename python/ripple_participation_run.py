0cd48ac126064aa78a1b1305efd0beee14c767e0
Ryan Harvey <ryanh412@gmail.com>
2022-01-20 10:29:31 -0500

    Create ripple_participation_run.py

---
+++ python/ripple_participation_run.py
@@ -0,0 +1,163 @@
+import pandas as pd
+import numpy as np 
+import functions,loading
+import nelpy as nel  # main nelpy imports
+import os
+import multiprocessing
+from joblib import Parallel, delayed
+
+def compress_repeated_epochs(epoch_df):
+    match = np.zeros([epoch_df.environment.shape[0]])
+    match[match == 0] = np.nan
+    for i,ep in enumerate(epoch_df.environment[:-1]):
+        if np.isnan(match[i]):
+            # find match in current and next epoch
+            if ep == epoch_df.environment.iloc[i+1]:
+                match[i:i+2] = i
+                # given match, see if there are more matches
+                for match_i in np.arange(1,epoch_df.environment[:-1].shape[0]):
+                    if i+1+match_i == epoch_df.environment.shape[0]:
+                        break
+                    if ep == epoch_df.environment.iloc[i+1+match_i]:
+
+                        match[i:i+1+match_i+1] = i
+                    else:
+                        break
+
+    for i in range(len(match)):
+        if np.isnan(match[i]):
+            match[i] = (i+1)*2000 # make nans large numbers that are unlikely to be real epoch
+
+    # iter through each epoch indicator to get start and stop
+    results = pd.DataFrame()
+    no_nan_match = match[~np.isnan(match)]
+    for m in pd.unique(no_nan_match):
+        temp_dict = {'name': epoch_df[match==m].name.iloc[0],
+                    'startTime':epoch_df[match==m].startTime.iloc[0],
+                    'stopTime':epoch_df[match==m].stopTime.iloc[-1],
+                    'environment':epoch_df[match==m].environment.iloc[0],
+                    'behavioralParadigm':epoch_df[match==m].behavioralParadigm.iloc[0],
+                    }
+        temp_df = pd.DataFrame.from_dict(temp_dict,orient='index').T
+
+        results = results.append(temp_df,ignore_index=True)
+    return results
+    
+def get_participation(st,ripple_epochs):
+    # get participation prob.
+    # make matrix n rows (units) by n cols (ripple epochs)
+    unit_mat = np.zeros((st.n_units,ripple_epochs.n_intervals))
+    for i,event in enumerate(st):
+        unit_mat[:,i] = (event.n_events>0)*1
+    return unit_mat
+
+def main_analysis(basepath,cell_metrics,st_unit,ripple_epochs,behavioral_epochs,epoch_df):
+
+    # create empty dataframe to add metrics from each epoch
+    df_save = pd.DataFrame()
+
+    # create spike train object with ripple epochs
+    st_unit_rip = st_unit[ripple_epochs]
+
+    epochs = epoch_df.environment
+    familiarity = epoch_df.behavioralParadigm
+
+    for i,ep in enumerate(behavioral_epochs):
+        temp_save = pd.DataFrame()
+
+        unit_mat = get_participation(st_unit_rip[ep],
+                                        ripple_epochs[ep])
+
+        participation_prob = np.sum(unit_mat,axis=1) / unit_mat.shape[1]
+
+        try:
+            avg_fr_not_rip = st_unit[~ripple_epochs][ep].n_spikes / ep[~ripple_epochs].duration
+            avg_fr_in_rip = st_unit_rip[ep].n_spikes / st_unit_rip[ep].support.duration
+            n_spikes = st_unit[ep].n_spikes
+        except:
+            avg_fr_not_rip = np.nan
+            avg_fr_in_rip = np.nan
+            n_spikes = np.nan
+
+        if participation_prob.shape[0] == 0:
+            participation_prob = np.nan
+            avg_fr_not_rip = np.nan
+            avg_fr_in_rip = np.nan
+            n_spikes = np.nan
+
+        # if ripple_epochs[ep].n_intervals < 50:
+        #     participation_prob = np.nan
+
+        # package results and previously saved metrics
+        temp_save['UID'] = cell_metrics.UID
+        temp_save['basepath'] = basepath
+        temp_save['epoch'] = epochs[i]
+        temp_save['epoch_n'] = i
+        temp_save['familiarity'] = familiarity[i]
+        temp_save['deepSuperficial'] = cell_metrics.deepSuperficial
+        temp_save['brainRegion'] = cell_metrics.brainRegion
+        temp_save['putativeCellType'] = cell_metrics.putativeCellType
+        temp_save['participation_prob'] = participation_prob
+        temp_save['avg_fr_not_rip'] = avg_fr_not_rip
+        temp_save['avg_fr_in_rip'] = avg_fr_in_rip
+        temp_save['n_spikes'] = n_spikes
+        temp_save['session_dur'] = ep.duration
+        temp_save['n_ripples'] = ripple_epochs[ep].n_intervals
+
+        df_save = df_save.append(temp_save,ignore_index=True)
+
+    return df_save
+
+def session_loop(basepath,save_path):
+    save_file = os.path.join(save_path,basepath.replace(os.sep, "_").replace(":", "_")  + '.csv')
+    if os.path.exists(save_file):
+        return
+
+    cell_metrics,data,ripples,fs_dat = loading.load_basic_data(basepath)
+
+    restrict_idx = (
+                    (cell_metrics.putativeCellType == "Pyramidal Cell") &
+                    ((cell_metrics.brainRegion=="CA1") |
+                    (cell_metrics.brainRegion=="rCA1") |
+                    (cell_metrics.brainRegion=="lCA1")) &
+                    (cell_metrics.bad_unit == False) 
+                    )
+
+    # restrict cell metrics                      
+    cell_metrics = cell_metrics[restrict_idx]
+    
+    if cell_metrics.shape[0] == 0:
+        return
+
+    # get ripple epochs
+    ripple_epochs = nel.EpochArray([np.array([ripples.start,ripples.stop]).T])
+    try:
+        st_unit = nel.SpikeTrainArray(timestamps=np.array(data['spikes'],dtype=object)[restrict_idx], fs=fs_dat)
+    except:
+        st_unit = nel.SpikeTrainArray(timestamps=np.array(data['spikes'],dtype=object)[restrict_idx][0], fs=fs_dat)
+
+    # behavioral epochs
+    epoch_df = loading.load_epoch(basepath)
+    # some epochs will have repeating back to back sessions that are actually the same session
+    epoch_df = compress_repeated_epochs(epoch_df)
+
+    behavioral_epochs = nel.EpochArray([np.array([epoch_df.startTime,
+                                                    epoch_df.stopTime]).T])
+
+    df_save = main_analysis(basepath,cell_metrics,st_unit,ripple_epochs,behavioral_epochs,epoch_df)
+    df_save.to_csv(save_file)
+
+def participation_run(df,save_path,parallel=True):
+    # find sessions to run
+    basepaths = pd.unique(df.basepath)
+
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    if parallel:
+        num_cores = multiprocessing.cpu_count()         
+        processed_list = Parallel(n_jobs=num_cores)(delayed(session_loop)(basepath,save_path) for basepath in basepaths)
+    else:    
+        for basepath in basepaths:
+            print(basepath)
+            session_loop(basepath,save_path)       
