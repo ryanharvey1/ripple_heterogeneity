febd6c11e9a23103d60d6e46b824621b10b5d963
Ryan Harvey <ryanh412@gmail.com>
2022-01-21 14:27:44 -0500

    new func

---
+++ matlab/custom_oml/add_maze_names.m
@@ -0,0 +1,28 @@
+df = readtable('Z:\home\ryanh\projects\ripple_heterogeneity\sessions.csv');
+df = df(contains(df.basepath,'OML'),:);
+
+for i = 1:length(unique(df.basepath))
+    basepath = df.basepath{i};
+    disp(basepath)
+    main(basepath)
+end
+
+function main(basepath)
+basename = basenameFromBasepath(basepath);
+load(fullfile(basepath,[basename,'.session.mat']))
+gui_session(session)
+% for ep = 1:length(session.epochs)
+%     if contains(session.epochs{ep}.name,'sleep','IgnoreCase',true)
+%         session.epochs{ep}.environment = 'sleep';
+%     elseif contains(session.epochs{ep}.name,'pre','IgnoreCase',true)
+%         session.epochs{ep}.environment = 'sleep';
+%     elseif contains(session.epochs{ep}.name,'post','IgnoreCase',true)
+%         session.epochs{ep}.environment = 'sleep';
+%     else
+%         disp(session.epochs{ep}.name)
+%         session.epochs{ep}.environment = 'wmaze';
+%     end
+% end
+% save(fullfile(basepath,[basename,'.session.mat']),'session')
+
+end
