dbb02e8c8b96af63f9642121d3a570ee6a7c7b61
Ryan Harvey <ryanh412@gmail.com>
2022-01-21 14:27:25 -0500

    compress only sleep

--- python/ripple_participation_run.py
+++ python/ripple_participation_run.py
@@ -7,12 +7,15 @@ import multiprocessing
 from joblib import Parallel, delayed
 
 def compress_repeated_epochs(epoch_df):
+    """
+    Compress back to back sleep epoch
+    """
     match = np.zeros([epoch_df.environment.shape[0]])
     match[match == 0] = np.nan
     for i,ep in enumerate(epoch_df.environment[:-1]):
         if np.isnan(match[i]):
             # find match in current and next epoch
-            if ep == epoch_df.environment.iloc[i+1]:
+            if (ep == epoch_df.environment.iloc[i+1]) & (ep == 'sleep'):
                 match[i:i+2] = i
                 # given match, see if there are more matches
                 for match_i in np.arange(1,epoch_df.environment[:-1].shape[0]):
