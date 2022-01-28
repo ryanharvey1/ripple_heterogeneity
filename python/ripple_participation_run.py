e45f8b1a91b85063c28f8619ed3f0187e6e0c29f
Ryan Harvey <ryanh412@gmail.com>
2022-01-28 15:43:26 -0500

    added state epochs

--- python/ripple_participation_run.py
+++ python/ripple_participation_run.py
@@ -16,13 +16,58 @@ def get_participation(st,ripple_epochs):
         unit_mat[:,i] = (event.n_events>0)*1
     return unit_mat
 
-def main_analysis(basepath,cell_metrics,st_unit,ripple_epochs,behavioral_epochs,epoch_df):
+def get_epoched_values(st_unit,st_unit_rip,st_unit_no_rip,ep,ripple_epochs,state_epoch=False):
+
+    if state_epoch == False:
+        unit_mat = get_participation(st_unit_rip[ep],
+                                            ripple_epochs[ep])
+        participation_prob = np.sum(unit_mat,axis=1) / unit_mat.shape[1]
+
+        avg_fr_not_rip = st_unit_no_rip[ep].n_spikes / ep[~ripple_epochs].duration
+        avg_fr_in_rip = st_unit_rip[ep].n_spikes / st_unit_rip[ep].support.duration
+        n_spikes = st_unit[ep].n_spikes
+    else:
+        unit_mat = get_participation(st_unit_rip[ep][state_epoch],
+                                            ripple_epochs[ep][state_epoch])
+        participation_prob = np.sum(unit_mat,axis=1) / unit_mat.shape[1]
+
+        try:
+            avg_fr_not_rip = (st_unit_no_rip[ep][state_epoch].n_spikes /
+                                ep[~ripple_epochs][state_epoch].duration)
+        except:
+            avg_fr_not_rip = np.nan
+        try:    
+            avg_fr_in_rip = (st_unit_rip[ep][state_epoch].n_spikes /
+                                st_unit_rip[ep][state_epoch].support.duration)
+        except:
+            avg_fr_in_rip = np.nan
+        try:
+            n_spikes = st_unit[ep][state_epoch].n_spikes
+        except:
+            n_spikes = np.nan
+
+    if participation_prob.shape[0] == 0:
+        participation_prob = np.nan
+
+    return participation_prob,avg_fr_not_rip,avg_fr_in_rip,n_spikes
+
+def main_analysis(
+                    basepath,
+                    cell_metrics,
+                    st_unit,
+                    ripple_epochs,
+                    behavioral_epochs,
+                    epoch_df,
+                    nrem_epochs,
+                    wake_epochs
+                    ):
 
     # create empty dataframe to add metrics from each epoch
     df_save = pd.DataFrame()
 
     # create spike train object with ripple epochs
     st_unit_rip = st_unit[ripple_epochs]
+    st_unit_no_rip = st_unit[~ripple_epochs]
 
     epochs = epoch_df.environment
     familiarity = epoch_df.behavioralParadigm
@@ -30,25 +75,26 @@ def main_analysis(basepath,cell_metrics,st_unit,ripple_epochs,behavioral_epochs,
     for i,ep in enumerate(behavioral_epochs):
         temp_save = pd.DataFrame()
 
-        unit_mat = get_participation(st_unit_rip[ep],
-                                        ripple_epochs[ep])
+        (
+        participation_prob,
+        avg_fr_not_rip,
+        avg_fr_in_rip,
+        n_spikes
+        ) = get_epoched_values(st_unit,st_unit_rip,st_unit_no_rip,ep,ripple_epochs)
 
-        participation_prob = np.sum(unit_mat,axis=1) / unit_mat.shape[1]
+        (
+        participation_prob_nrem,
+        avg_fr_not_rip_nrem,
+        avg_fr_in_rip_nrem,
+        n_spikes_nrem
+        ) = get_epoched_values(st_unit,st_unit_rip,st_unit_no_rip,ep,ripple_epochs,nrem_epochs)
 
-        try:
-            avg_fr_not_rip = st_unit[~ripple_epochs][ep].n_spikes / ep[~ripple_epochs].duration
-            avg_fr_in_rip = st_unit_rip[ep].n_spikes / st_unit_rip[ep].support.duration
-            n_spikes = st_unit[ep].n_spikes
-        except:
-            avg_fr_not_rip = np.nan
-            avg_fr_in_rip = np.nan
-            n_spikes = np.nan
-
-        if participation_prob.shape[0] == 0:
-            participation_prob = np.nan
-            avg_fr_not_rip = np.nan
-            avg_fr_in_rip = np.nan
-            n_spikes = np.nan
+        (
+        participation_prob_wake,
+        avg_fr_not_rip_wake,
+        avg_fr_in_rip_wake,
+        n_spikes_wake
+        ) = get_epoched_values(st_unit,st_unit_rip,st_unit_no_rip,ep,ripple_epochs,wake_epochs)
 
         # package results and previously saved metrics
         temp_save['UID'] = cell_metrics.UID
@@ -60,11 +106,23 @@ def main_analysis(basepath,cell_metrics,st_unit,ripple_epochs,behavioral_epochs,
         temp_save['brainRegion'] = cell_metrics.brainRegion
         temp_save['putativeCellType'] = cell_metrics.putativeCellType
         temp_save['participation_prob'] = participation_prob
+        temp_save['participation_prob_nrem'] = participation_prob_nrem
+        temp_save['participation_prob_wake'] = participation_prob_wake
         temp_save['avg_fr_not_rip'] = avg_fr_not_rip
         temp_save['avg_fr_in_rip'] = avg_fr_in_rip
         temp_save['n_spikes'] = n_spikes
+        temp_save['avg_fr_not_rip_nrem'] = avg_fr_not_rip_nrem
+        temp_save['avg_fr_in_rip_nrem'] = avg_fr_in_rip_nrem
+        temp_save['n_spikes_nrem'] = n_spikes_nrem
+        temp_save['avg_fr_not_rip_wake'] = avg_fr_not_rip_wake
+        temp_save['avg_fr_in_rip_wake'] = avg_fr_in_rip_wake
+        temp_save['n_spikes_wake'] = n_spikes_wake
         temp_save['session_dur'] = ep.duration
+        temp_save['session_dur_nrem'] = ep[nrem_epochs].duration
+        temp_save['session_dur_wake'] = ep[wake_epochs].duration
         temp_save['n_ripples'] = ripple_epochs[ep].n_intervals
+        temp_save['n_ripples_nrem'] = ripple_epochs[ep][nrem_epochs].n_intervals
+        temp_save['n_ripples_wake'] = ripple_epochs[ep][wake_epochs].n_intervals
 
         df_save = df_save.append(temp_save,ignore_index=True)
 
@@ -110,7 +168,20 @@ def session_loop(basepath,save_path):
     behavioral_epochs = nel.EpochArray([np.array([epoch_df.startTime,
                                                     epoch_df.stopTime]).T])
 
-    df_save = main_analysis(basepath,cell_metrics,st_unit,ripple_epochs,behavioral_epochs,epoch_df)
+    state_dict = loading.load_SleepState_states(basepath)
+    nrem_epochs = nel.EpochArray(state_dict['NREMstate'])
+    wake_epochs = nel.EpochArray(state_dict['WAKEstate'])
+
+    df_save = main_analysis(
+                            basepath,
+                            cell_metrics,
+                            st_unit,
+                            ripple_epochs,
+                            behavioral_epochs,
+                            epoch_df,
+                            nrem_epochs,
+                            wake_epochs
+                            )
     df_save.to_csv(save_file)
 
 def participation_run(df,save_path,parallel=True):
