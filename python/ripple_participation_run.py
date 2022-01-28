2ccc4c42e8b0e556f1888e0e4624d28ed325fb6f
Ryan Harvey <ryanh412@gmail.com>
2022-01-28 12:52:27 -0500

    added def to real .py

--- python/ripple_participation_run.py
+++ python/ripple_participation_run.py
@@ -5,46 +5,8 @@ import nelpy as nel  # main nelpy imports
 import os
 import multiprocessing
 from joblib import Parallel, delayed
+import compress_repeated_epochs
 
-def compress_repeated_epochs(epoch_df):
-    """
-    Compress back to back sleep epoch
-    """
-    match = np.zeros([epoch_df.environment.shape[0]])
-    match[match == 0] = np.nan
-    for i,ep in enumerate(epoch_df.environment[:-1]):
-        if np.isnan(match[i]):
-            # find match in current and next epoch
-            if (ep == epoch_df.environment.iloc[i+1]) & (ep == 'sleep'):
-                match[i:i+2] = i
-                # given match, see if there are more matches
-                for match_i in np.arange(1,epoch_df.environment[:-1].shape[0]):
-                    if i+1+match_i == epoch_df.environment.shape[0]:
-                        break
-                    if ep == epoch_df.environment.iloc[i+1+match_i]:
-
-                        match[i:i+1+match_i+1] = i
-                    else:
-                        break
-
-    for i in range(len(match)):
-        if np.isnan(match[i]):
-            match[i] = (i+1)*2000 # make nans large numbers that are unlikely to be real epoch
-
-    # iter through each epoch indicator to get start and stop
-    results = pd.DataFrame()
-    no_nan_match = match[~np.isnan(match)]
-    for m in pd.unique(no_nan_match):
-        temp_dict = {'name': epoch_df[match==m].name.iloc[0],
-                    'startTime':epoch_df[match==m].startTime.iloc[0],
-                    'stopTime':epoch_df[match==m].stopTime.iloc[-1],
-                    'environment':epoch_df[match==m].environment.iloc[0],
-                    'behavioralParadigm':epoch_df[match==m].behavioralParadigm.iloc[0],
-                    }
-        temp_df = pd.DataFrame.from_dict(temp_dict,orient='index').T
-
-        results = results.append(temp_df,ignore_index=True)
-    return results
     
 def get_participation(st,ripple_epochs):
     # get participation prob.
@@ -145,8 +107,8 @@ def session_loop(basepath,save_path):
     if not ('behavioralParadigm' in epoch_df.columns):
         epoch_df['behavioralParadigm'] = 'unknown'
 
-    # some epochs will have repeating back to back sessions that are actually the same session
-    epoch_df = compress_repeated_epochs(epoch_df)
+    # some epochs will have repeating back to back sleep sessions that are actually the same session
+    epoch_df = compress_repeated_epochs.compress_repeated_epochs(epoch_df)
 
     behavioral_epochs = nel.EpochArray([np.array([epoch_df.startTime,
                                                     epoch_df.stopTime]).T])
