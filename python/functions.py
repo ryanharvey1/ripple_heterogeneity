48574e9b98d35a489ba8f7de8587fd31e61861a2
Ryan Harvey <ryanh412@gmail.com>
2022-02-17 11:23:08 -0500

    convert to np.array

--- python/functions.py
+++ python/functions.py
@@ -354,8 +354,8 @@ def get_participation(st,event_starts,event_stops,par_type='binary'):
         ripple_epochs: ripple events in nelpy epoch object 
         par_type: participation type (counts, binary, firing_rate)
     """
-    unit_mat = get_participation_(fix_array_or_list(list(st)),event_starts,event_stops)
-    
+    unit_mat = get_participation_(fix_array_or_list(list(st)),np.array(event_starts),np.array(event_stops))
+
     if par_type == 'counts':
         pass
     elif par_type == 'binary':
