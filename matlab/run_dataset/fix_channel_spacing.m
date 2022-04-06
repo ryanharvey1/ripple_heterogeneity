5df177bc29ed2ea72b4086d8f729605c4fe6e42b
Ryan Harvey <ryanh412@gmail.com>
2022-04-06 11:55:23 -0400

    building out pipline

--- matlab/run_dataset/fix_channel_spacing.m
+++ matlab/run_dataset/fix_channel_spacing.m
@@ -3,7 +3,10 @@
 %
 %   most channels are ~20um apart
 %
-if ~exist('Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv','file')
+% get data sheet
+if exist('Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv','file')
+    probe_df = readtable('Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv');
+else
     df = readtable('Z:\home\ryanh\projects\ripple_heterogeneity\sessions.csv');
     df_probe = table();
     for i = 1:length(df.basepath)
@@ -26,16 +29,21 @@ if ~exist('Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv','file')
     probe_df = cell2table(vertcat(C{:}),...
         "VariableNames",["basepath" "animal" "probesLayout" "probesVerticalSpacing"]);
     writetable(probe_df,'Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv')
-else
-    probe_df = readtable('Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv');
 end
 
+% iter over each session
 for i = 1:length(probe_df.basepath)
-    if probe_df.fixed(i) == true
-        continue
-    end
-    basepath = probe_df.basepath{i};
-    real_spacing = probe_df.real_spacing(i);
+%     if probe_df.fixed(i) == true
+%         continue
+%     end
+    fix_it(probe_df.basepath{i},probe_df.real_spacing(i))
+    probe_df.fixed(i) = true;
+    writetable(probe_df,'Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv')
+end
+
+% func to fix channel dist and prop changes throughout all data types
+function fix_it(basepath,real_spacing)
+
     basename = basenameFromBasepath(basepath);
     load(fullfile(basepath,[basename,'.session.mat']))
     load(fullfile(basepath,[basename,'.deepSuperficialfromRipple.channelinfo.mat']))
@@ -43,20 +51,43 @@ for i = 1:length(probe_df.basepath)
     load(fullfile(basepath,[basename,'.spikes.cellinfo.mat']))
 
     for group_i = 1:length(deepSuperficialfromRipple.ripple_channels)
+        % locate channels in group
         channels = deepSuperficialfromRipple.ripple_channels{group_i};
-        
-        [a,b] = ismember(channels,deepSuperficialfromRipple.channel);
-        
-        ratio = real_spacing / mode(diff(deepSuperficialfromRipple.channelDistance(b)));
+        [~,b] = ismember(channels,deepSuperficialfromRipple.channel);
+        % find current offset
+        % sort to account for mapping differences
+        sorted_channels = sort(deepSuperficialfromRipple.channelDistance(b));
+        current_channel_offset = mode(diff(sorted_channels));
+        % find ratio between real and current
+        ratio = real_spacing / current_channel_offset;
+        % adjust current by ratio to make real channel dist
         deepSuperficialfromRipple.channelDistance(b) =...
             deepSuperficialfromRipple.channelDistance(b) * ratio;
     end
-    session.animal.probeImplants{1, 1}.verticalSpacing = real_spacing;
-    session.analysisTags.probesVerticalSpacing = real_spacing;
     
+    % assign spacing meta data, this is hidden all over the place
+    % in session
+    session.animal.probeImplants{1, 1}.verticalSpacing = real_spacing;
+    session.animal.probeImplants{1, 1}.supplier = [];
+    session.animal.probeImplants{1, 1}.descriptiveName = [];
+    session.analysisTags.probesVerticalSpacing = real_spacing;
+    % in deepSuperficialfromRipple file
+    deepSuperficialfromRipple.processinginfo.params.verticalSpacing = real_spacing;
+    % in cell metrics
+    cell_metrics.general.SWR = deepSuperficialfromRipple;
+    cell_metrics.general.SWR_batch = deepSuperficialfromRipple;
+    cell_metrics.general.processinginfo.deepSuperficialfromRipple.params.verticalSpacing = real_spacing;
     cell_metrics.general.deepSuperficial_file =...
         fullfile(basepath,[basename,'.deepSuperficialfromRipple.channelinfo.mat']);
+    %     cell_metrics.general.processinginfo.params.session.analysisTags.probesVerticalSpacing = real_spacing;
+    cell_metrics.general.processinginfo.params.session = session;
     
+    % for some reason, the basename can change... fix this here
+    if ~contains(cell_metrics.general.basename,basename)
+        cell_metrics.general.basename = basename;
+    end
+    
+    % update cell locations
     for j = 1:cell_metrics.general.cellCount
         if isnan(spikes.maxWaveformCh1(j))
             cell_metrics.deepSuperficial(j) = {'Unknown'};
@@ -69,13 +100,9 @@ for i = 1:length(probe_df.basepath)
         end
     end
     
-    probe_df.fixed(i) = true;
-    
     save(fullfile(basepath,[basename,'.deepSuperficialfromRipple.channelinfo.mat']),'deepSuperficialfromRipple')
     save(fullfile(basepath,[basename,'.session.mat']),'session')
     save(fullfile(basepath,[basename,'.cell_metrics.cellinfo.mat']),'cell_metrics')
-    writetable(probe_df,'Z:\home\ryanh\projects\ripple_heterogeneity\probe_df.csv')
-
 end
 
 %         [a,b] = ismember(channels,deepSuperficialfromRipple.channel);
