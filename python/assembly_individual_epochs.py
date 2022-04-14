308891171f2062246df95744064f0845a2d27ff5
Ryan Harvey <ryanh412@gmail.com>
2022-04-14 14:39:04 -0400

    added docs and more bug fix

--- python/assembly_individual_epochs.py
+++ python/assembly_individual_epochs.py
@@ -1,6 +1,7 @@
 import nelpy as nel
 import functions, loading
 import sys
+import glob
 
 sys.path.append(r"D:\github\neurocode\reactivation\assemblies")
 import assembly
@@ -31,7 +32,18 @@ def get_z_t(st, ds=0.001):
 def main_analysis(
     st, ripple_epochs, behavioral_epochs, epoch_df, nrem_epochs, wake_epochs, dt=0.010
 ):
-
+    """
+    Input:
+        st: spike train
+        ripple_epochs: ripple epochs
+        behavioral_epochs: behavioral epochs
+        epoch_df: epoch dataframe
+        nrem_epochs: nrem epochs
+        wake_epochs: wake epochs
+        dt: time bin size
+    Output:
+        results: dictionary of results
+    """
     # spike times within ripples
     st_rip = st[ripple_epochs]
 
@@ -47,6 +59,10 @@ def main_analysis(
             temp_st = st_rip[wake_epochs][ep]
         # check if temp_st is empty
         if temp_st.data is None:
+            patterns.append(None)
+            significance.append(None)
+            zactmat.append(None)
+            env.append(epoch_df.environment.iloc[i])
             continue
         # extract assembly patterns
         (patterns_, significance_, zactmat_) = assembly.runPatterns(
@@ -70,7 +86,13 @@ def main_analysis(
 
 
 def session_loop(basepath, save_path, rip_window=0.050):
-
+    """
+    Loop through each session
+    Input:
+        basepath: path to the session
+        save_path: path to save the results
+        rip_window: time window to define ripples
+    """
     save_file = os.path.join(
         save_path, basepath.replace(os.sep, "_").replace(":", "_") + ".pkl"
     )
@@ -88,12 +110,14 @@ def session_loop(basepath, save_path, rip_window=0.050):
 
     # get ripple epochs
     ripple_epochs = nel.EpochArray([np.array([ripples.start, ripples.stop]).T])
-    ripple_epochs = ripple_epochs.expand(rip_window,direction="both")
+    ripple_epochs = ripple_epochs.expand(rip_window, direction="both")
 
     # behavioral epochs
     epoch_df = loading.load_epoch(basepath)
 
-    idx = functions.find_epoch_pattern(epoch_df.environment,["sleep","linear","sleep"])
+    idx = functions.find_epoch_pattern(
+        epoch_df.environment, ["sleep", "linear", "sleep"]
+    )
     epoch_df = epoch_df[idx[0]]
 
     behavioral_epochs = nel.EpochArray(
@@ -120,6 +144,9 @@ def session_loop(basepath, save_path, rip_window=0.050):
 
 
 def assembly_run(df, save_path, parallel=True):
+    """
+    Run the assembly analysis on all sessions in the dataframe
+    """
     # find sessions to run
     basepaths = pd.unique(df.basepath)
 
@@ -135,3 +162,49 @@ def assembly_run(df, save_path, parallel=True):
         for basepath in basepaths:
             print(basepath)
             session_loop(basepath, save_path)
+
+
+def load_assem_epoch_data(save_path):
+    """
+    Loads the assembly epoch data from the save_path
+    """
+
+    sessions = glob.glob(save_path + os.sep + "*.pkl")
+
+    assem_epoch_df = pd.DataFrame()
+
+    for session in sessions:
+        assem_epoch_df_temp = pd.DataFrame()
+        prob_sig_member = []
+        n_members = []
+        n_assemblies = []
+        n_cells = []
+        epoch = []
+        with open(session, "rb") as f:
+            results = pickle.load(f)
+
+            for i, pattern_ep in enumerate(results["patterns"]):
+                (
+                    patterns_keep,
+                    is_member_keep,
+                    keep_assembly,
+                    is_member,
+                ) = functions.find_sig_assemblies(pattern_ep)
+                prob_sig_member.append(np.mean(is_member_keep))
+                n_members.append(is_member_keep.sum())
+                n_assemblies.append(patterns_keep.shape[0])
+                n_cells.append(patterns_keep.shape[1])
+                epoch.append(results['env'][i])
+
+            assem_epoch_df_temp["prob_sig_member"] = prob_sig_member
+            assem_epoch_df_temp["n_members"] = n_members
+            assem_epoch_df_temp["n_assemblies"] = n_assemblies
+            assem_epoch_df_temp["n_cells"] = n_cells
+            assem_epoch_df_temp["epoch"] = epoch
+            assem_epoch_df_temp["basepath"] = results["basepath"]
+
+        assem_epoch_df = pd.concat(
+            [assem_epoch_df, assem_epoch_df_temp], ignore_index=True
+        )
+
+    return assem_epoch_df
