8ba974f55fba9b3df16403475b0c74fda974b6d5
Ryan Harvey <ryanh412@gmail.com>
2022-04-18 18:40:39 -0400

    Create test_similarity_index.py

---
+++ tests/test_similarity_index.py
@@ -0,0 +1,23 @@
+import numpy as np
+from ripple_heterogeneity.assembly import similarity_index
+import pytest
+
+def test_similarityindex():
+    patterns = np.array(
+        [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+    )
+    si, combos, pvalues = similarity_index.similarity_index(patterns)
+    assert si[0] == 10
+    assert combos[0,0] == 0
+    assert pvalues[0] == 1
