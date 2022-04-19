7cd8798f364d9a1c5ba8e76b12903992d3a9b13b
Ryan Harvey <ryanh412@gmail.com>
2022-04-19 10:16:02 -0400

    fixed incorrect test cases

--- tests/test_similarity_index.py
+++ tests/test_similarity_index.py
@@ -3,21 +3,9 @@ from ripple_heterogeneity.assembly import similarity_index
 import pytest
 
 def test_similarityindex():
-    patterns = np.array(
-        [
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        ]
-    )
+    patterns = [np.random.normal(0, .1, 100) for _ in range(10)]
+
     si, combos, pvalues = similarity_index.similarity_index(patterns)
-    assert si[0] == 10
+    assert all(i <= 1 for i in si)
     assert combos[0,0] == 0
-    assert pvalues[0] == 1
+    assert all(i <= 1 for i in pvalues)
