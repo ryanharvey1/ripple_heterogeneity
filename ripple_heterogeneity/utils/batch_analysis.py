4f6af401eac24d7fcdbdadf1f697dedde05e1090
Ryan Harvey <ryanh412@gmail.com>
2022-04-26 23:21:26 -0400

    added more doc

--- ripple_heterogeneity/utils/batch_analysis.py
+++ ripple_heterogeneity/utils/batch_analysis.py
@@ -10,15 +10,17 @@ def main_loop(basepath, save_path, func, overwrite, **kwargs):
     """
     main_loop: file management & run function
     Inputs:
-        basepath: str
-        save_path: str
-        func: function
-        overwrite: bool
-        kwargs: dict
+        basepath: str path to session
+        save_path: str path to save results to (will be created if it doesn't exist)
+        func: function to run on each basepath in df (see run)
+        overwrite: bool whether to overwrite existing files in save_path
+        kwargs: dict of keyword arguments to pass to func (see run)
     """
+    # get file name from basepath
     save_file = os.path.join(
         save_path, basepath.replace(os.sep, "_").replace(":", "_") + ".pkl"
     )
+    # if file exists and overwrite is False, skip
     if os.path.exists(save_file) & ~overwrite:
         return
 
@@ -33,28 +35,32 @@ def main_loop(basepath, save_path, func, overwrite, **kwargs):
 def run(df, save_path, func, parallel=True, verbose=False, overwrite=False, **kwargs):
     """
     Inputs:
-        df: pandas dataframe
-        save_path: str
-        func: function
-        parallel: bool
-        verbose: bool
-        overwrite: bool
-        kwargs: dict
+        df: pandas dataframe with basepath column
+        save_path: str path to save results to (will be created if it doesn't exist)
+        func: function to run on each basepath in df (see main_loop)
+        parallel: bool whether to run in parallel or not
+        verbose: bool whether to print progress
+        overwrite: bool whether to overwrite existing files in save_path
+        kwargs: dict of keyword arguments to pass to func
     """
     # find sessions to run
     basepaths = pd.unique(df.basepath)
-
+    # create save_path if it doesn't exist
     if not os.path.exists(save_path):
         os.mkdir(save_path)
-
+    # run in parallel if parallel is True
     if parallel:
+        # get number of cores
         num_cores = multiprocessing.cpu_count()
+        # run in parallel
         processed_list = Parallel(n_jobs=num_cores)(
             delayed(main_loop)(basepath, save_path, func, overwrite, **kwargs)
             for basepath in tqdm(basepaths)
         )
     else:
+        # run in serial
         for basepath in tqdm(basepaths):
             if verbose:
                 print(basepath)
+            # run main_loop on each basepath in df
             main_loop(basepath, save_path, func, overwrite, **kwargs)
