02be4af596f4131ce6463d7107a21d698c86f1c4
Ryan Harvey <ryanh412@gmail.com>
2022-04-29 16:07:02 -0400

    update for oml

--- ripple_heterogeneity/formatting_data/custom_oml/add_maze_names.m
+++ ripple_heterogeneity/formatting_data/custom_oml/add_maze_names.m
@@ -10,19 +10,26 @@ end
 function main(basepath)
 basename = basenameFromBasepath(basepath);
 load(fullfile(basepath,[basename,'.session.mat']))
-gui_session(session)
-% for ep = 1:length(session.epochs)
-%     if contains(session.epochs{ep}.name,'sleep','IgnoreCase',true)
-%         session.epochs{ep}.environment = 'sleep';
-%     elseif contains(session.epochs{ep}.name,'pre','IgnoreCase',true)
-%         session.epochs{ep}.environment = 'sleep';
-%     elseif contains(session.epochs{ep}.name,'post','IgnoreCase',true)
-%         session.epochs{ep}.environment = 'sleep';
-%     else
-%         disp(session.epochs{ep}.name)
-%         session.epochs{ep}.environment = 'wmaze';
-%     end
-% end
-% save(fullfile(basepath,[basename,'.session.mat']),'session')
+% gui_session(session)
+for ep = 1:length(session.epochs)
+    if contains(session.epochs{ep}.name,'sleep','IgnoreCase',true)
+        session.epochs{ep}.environment = 'sleep';
+    elseif contains(session.epochs{ep}.name,'pre','IgnoreCase',true)
+        session.epochs{ep}.environment = 'sleep';
+    elseif contains(session.epochs{ep}.name,'post','IgnoreCase',true)
+        session.epochs{ep}.environment = 'sleep';
+    elseif contains(session.epochs{ep}.name,'baseline','IgnoreCase',true)
+        session.epochs{ep}.environment = 'cheeseboard';
+    elseif contains(session.epochs{ep}.name,'linear','IgnoreCase',true)
+        session.epochs{ep}.environment = 'linear';
+    elseif ~isfield(session.epochs{ep},'environment') 
+        disp(session.epochs{ep}.name)
+        session.epochs{ep}.environment = 'cheeseboard';
+    elseif isempty(session.epochs{ep}.environment)
+        disp(session.epochs{ep}.name)
+        session.epochs{ep}.environment = 'cheeseboard';
+    end
+end
+save(fullfile(basepath,[basename,'.session.mat']),'session')
 
 end
