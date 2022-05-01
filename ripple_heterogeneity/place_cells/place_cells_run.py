06cb61ec6a7cb4aa8afb1e2d7b32a082120dfd03
Ryan Harvey <ryanh412@gmail.com>
2022-05-01 19:23:35 -0400

    reworked to get 1d place fields from linear both dirs

--- ripple_heterogeneity/place_cells/place_cells_run.py
+++ ripple_heterogeneity/place_cells/place_cells_run.py
@@ -82,10 +82,32 @@ def surrogate_test_spatial_info(ts, x, y, s, n_shuff=500, bin_width=3):
     return pvalue(null_ic, obs_ic), null_ic, obs_ic
 
 
-def run(basepath, n_shuff=500, min_session_duration=5, bin_width=3, speed_thres=4):
+def get_maps_and_score(cell_id, st_run, x, y, ts, bin_width, n_shuff):
+
+    ratemaps, occupancies = get_ratemap(
+        ts, x, y, st_run.data[cell_id], bin_width=bin_width
+    )
+
+    pvals, null_ics, spatial_infos = surrogate_test_spatial_info(
+        ts, x, y, st_run.data[cell_id], bin_width=bin_width, n_shuff=n_shuff
+    )
+
+    return ratemaps, occupancies, pvals, null_ics, spatial_infos
+
+
+def run(
+    basepath,
+    n_shuff=500,
+    min_session_duration=5,
+    bin_width=3,
+    speed_thres=4,
+    epochs_to_skip=["sleep", "wheel", "cheeseboard"],
+    brainRegion = "CA1",
+    cell_type = "Pyr",
+):
 
     st_unit, cell_metrics = loading.load_spikes(
-        basepath, brainRegion="CA1", putativeCellType="Pyr"
+        basepath, brainRegion=brainRegion, putativeCellType=cell_type
     )
     if cell_metrics.shape[0] == 0:
         return
@@ -99,10 +121,11 @@ def run(basepath, n_shuff=500, min_session_duration=5, bin_width=3, speed_thres=
         return
 
     epoch_df = loading.load_epoch(basepath)
-    # remove sleep and wheel running
-    epoch_df = epoch_df[
-        (epoch_df.environment != "sleep") & (epoch_df.environment != "wheel")
-    ]
+
+    # remove sleep and wheel running and others
+    for epoch in epochs_to_skip:
+        epoch_df = epoch_df[epoch_df.environment != epoch]
+
     # remove sessions < 5 minutes
     epoch_df = epoch_df[
         (epoch_df.stopTime - epoch_df.startTime) / 60 > min_session_duration
@@ -164,6 +187,7 @@ def run(basepath, n_shuff=500, min_session_duration=5, bin_width=3, speed_thres=
     ys = []
     tss = []
     st = []
+    linear_dir = []
     for ep_i, ep in enumerate(beh_epochs):
 
         x = pos[ep].data[0, :]
@@ -186,40 +210,103 @@ def run(basepath, n_shuff=500, min_session_duration=5, bin_width=3, speed_thres=
         st_run = st_unit[ep][run_epochs]
         pos_run = pos[ep][run_epochs]
 
-        ts = pos_run.abscissa_vals
-        x = pos_run.data[0, :]
-        y = pos_run.data[1, :]
-
         if st_run.n_active == 0:
             print("no spk data in epoch")
             continue
 
-        # iter over cells
-        for cell_id in range(st_run.data.shape[0]):
-
-            ratemap, occupancy = get_ratemap(
-                ts, x, y, st_run.data[cell_id], bin_width=bin_width
+        if "linear" in epoch_df.iloc[ep_i].environment:
+            (outbound_epochs, inbound_epochs) = functions.get_linear_track_lap_epochs(
+                pos_run[ep].abscissa_vals, pos_run.data[0], newLapThreshold=20
             )
-            ratemaps.append(ratemap)
-            occupancies.append(occupancy)
+            for dir_epoch_i, dir_epoch in enumerate([outbound_epochs, inbound_epochs]):
+                ts = pos_run[dir_epoch].abscissa_vals
+                x = pos_run[dir_epoch].data[0, :]
+                y = np.ones_like(x)
 
-            pval, null_ic, spatial_info = surrogate_test_spatial_info(
-                ts, x, y, st_run.data[cell_id], bin_width=bin_width, n_shuff=n_shuff
-            )
-            pvals.append(pval)
-            null_ics.append(null_ic)
-            spatial_infos.append(spatial_info)
+                for cell_id in range(st_run.data.shape[0]):
+                    (
+                        ratemap_,
+                        occupancy_,
+                        pval,
+                        null_ic,
+                        spatial_info,
+                    ) = get_maps_and_score(
+                        cell_id, st_run, x, y, ts, bin_width, n_shuff
+                    )
 
-            UID.append(cell_metrics.iloc[cell_id].UID)
-            env.append(epoch_df.iloc[ep_i].environment)
-            name.append(epoch_df.name.iloc[ep_i])
-            startTime.append(epoch_df.iloc[ep_i].startTime)
-            stopTime.append(epoch_df.iloc[ep_i].stopTime)
-            # epoch = pd.concat([epoch, epoch_df.iloc[ep_i]], ignore_index=True)
-            xs.append(x)
-            ys.append(y)
-            tss.append(ts)
-            st.append(st_run.data[cell_id])
+                    pvals.append(pval)
+                    null_ics.append(null_ic)
+                    spatial_infos.append(spatial_info)
+                    UID.append(cell_metrics.iloc[cell_id].UID)
+                    env.append(epoch_df.iloc[ep_i].environment)
+                    name.append(epoch_df.name.iloc[ep_i])
+                    startTime.append(epoch_df.iloc[ep_i].startTime)
+                    stopTime.append(epoch_df.iloc[ep_i].stopTime)
+                    xs.append(x)
+                    ys.append(y)
+                    tss.append(ts)
+                    st.append(st_run.data[cell_id])
+                    ratemaps.append(ratemap_)
+                    occupancies.append(occupancy_)
+                    linear_dir.append(dir_epoch_i)
+        else:
+            ts = pos_run.abscissa_vals
+            x = pos_run.data[0, :]
+            y = pos_run.data[1, :]
+
+            for cell_id in range(st_run.data.shape[0]):
+                (
+                    ratemap_,
+                    occupancy_,
+                    pval,
+                    null_ic,
+                    spatial_info,
+                ) = get_maps_and_score(cell_id, st_run, x, y, ts, bin_width, n_shuff)
+
+                pvals.append(pval)
+                null_ics.append(null_ic)
+                spatial_infos.append(spatial_info)
+                UID.append(cell_metrics.iloc[cell_id].UID)
+                env.append(epoch_df.iloc[ep_i].environment)
+                name.append(epoch_df.name.iloc[ep_i])
+                startTime.append(epoch_df.iloc[ep_i].startTime)
+                stopTime.append(epoch_df.iloc[ep_i].stopTime)
+                xs.append(x)
+                ys.append(y)
+                tss.append(ts)
+                st.append(st_run.data[cell_id])
+                ratemaps.append(ratemap_)
+                occupancies.append(occupancy_)
+                linear_dir.append(np.nan)
+
+                # epoch = pd.concat([epoch, epoch_temp], ignore_index=True)
+
+    #     # iter over cells
+    #     for cell_id in range(st_run.data.shape[0]):
+
+    #         ratemap, occupancy = get_ratemap(
+    #             ts, x, y, st_run.data[cell_id], bin_width=bin_width
+    #         )
+    #         ratemaps.append(ratemap)
+    #         occupancies.append(occupancy)
+
+    #         pval, null_ic, spatial_info = surrogate_test_spatial_info(
+    #             ts, x, y, st_run.data[cell_id], bin_width=bin_width, n_shuff=n_shuff
+    #         )
+    #         pvals.append(pval)
+    #         null_ics.append(null_ic)
+    #         spatial_infos.append(spatial_info)
+
+    #         UID.append(cell_metrics.iloc[cell_id].UID)
+    #         env.append(epoch_df.iloc[ep_i].environment)
+    #         name.append(epoch_df.name.iloc[ep_i])
+    #         startTime.append(epoch_df.iloc[ep_i].startTime)
+    #         stopTime.append(epoch_df.iloc[ep_i].stopTime)
+    #         # epoch = pd.concat([epoch, epoch_df.iloc[ep_i]], ignore_index=True)
+    #         xs.append(x)
+    #         ys.append(y)
+    #         tss.append(ts)
+    #         st.append(st_run.data[cell_id])
 
     epoch["UID"] = np.hstack(UID)
     epoch["environment"] = np.hstack(env)
@@ -228,6 +315,7 @@ def run(basepath, n_shuff=500, min_session_duration=5, bin_width=3, speed_thres=
     epoch["stopTime"] = np.hstack(stopTime)
     epoch["spatial_infos"] = np.hstack(spatial_infos)
     epoch["pvals"] = np.hstack(pvals)
+    epoch["linear_dir"] = np.hstack(linear_dir)
     epoch["basepath"] = basepath
 
     results = {}
