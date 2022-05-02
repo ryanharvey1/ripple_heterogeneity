7f315fb8e04b27b69a622caa8092132719178f6d
Ryan Harvey <ryanh412@gmail.com>
2022-05-02 11:10:16 -0400

    moved reclass to after main loop

--- ripple_heterogeneity/utils/add_new_deep_sup.py
+++ ripple_heterogeneity/utils/add_new_deep_sup.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 from ripple_heterogeneity.utils import loading
 
+
 def add_new_deep_sup_class(df, layer_dist=30):
     """
     Take df dataframe and update deepSuperficial classification
 
     """
-    LAYERDIST = layer_dist
 
-    def assign_region(df,cell_metrics_):
+    def assign_region(df, cell_metrics_):
         """
         Assign deepSuperficial classification based on distance from the pyramidal layer
         """
@@ -23,27 +23,27 @@ def add_new_deep_sup_class(df, layer_dist=30):
             df.loc[df.UID == uid, "deepSuperficialDistance"] = np.tile(
                 deepSuperficialDistance, sum(df.UID == uid)
             )
-
-        deep = -LAYERDIST
-        middle = [-LAYERDIST, LAYERDIST]
-        sup = LAYERDIST
-        df.loc[df.deepSuperficialDistance <= deep, "deepSuperficial"] = "Deep"
-        df.loc[
-            (df.deepSuperficialDistance > middle[0])
-            & (df.deepSuperficialDistance < middle[1]),
-            "deepSuperficial",
-        ] = "middle"
-        df.loc[df.deepSuperficialDistance >= sup, "deepSuperficial"] = "Superficial"
         return df
 
     cell_metrics = loading.load_all_cell_metrics(df.basepath.unique())
 
-    df_temp = pd.DataFrame()
+    df_out = pd.DataFrame()
     # iter over each unique basepath
     for basepath in df.basepath.unique():
-        df_temp = pd.concat(
-            [df_temp, assign_region(df[df.basepath == basepath].copy(),cell_metrics)],
+        df_out = pd.concat(
+            [df_out, assign_region(df[df.basepath == basepath].copy(), cell_metrics)],
             ignore_index=True,
         )
 
-    return df_temp
+    deep = -layer_dist
+    middle = [-layer_dist, layer_dist]
+    sup = layer_dist
+    df_out.loc[df_out.deepSuperficialDistance <= deep, "deepSuperficial"] = "Deep"
+    df_out.loc[
+        (df_out.deepSuperficialDistance > middle[0])
+        & (df_out.deepSuperficialDistance < middle[1]),
+        "deepSuperficial",
+    ] = "middle"
+    df_out.loc[df_out.deepSuperficialDistance >= sup, "deepSuperficial"] = "Superficial"
+
+    return df_out
