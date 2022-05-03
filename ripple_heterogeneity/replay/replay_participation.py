15afd034d7e96dfa715e00d9a79b43b72157f09c
Ryan Harvey <ryanh412@gmail.com>
2022-05-03 00:20:48 -0400

    add padding for participation and added other participation types

--- ripple_heterogeneity/replay/replay_participation.py
+++ ripple_heterogeneity/replay/replay_participation.py
@@ -6,7 +6,7 @@ from ripple_heterogeneity.utils import functions, loading, compress_repeated_epo
 import nelpy as nel
 
 
-def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05):
+def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05, partic_pad=0.05):
     """
     Compile replay participation for a session
     Inputs:
@@ -18,26 +18,32 @@ def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05):
     # select current session from replay_df
     replay_df = replay_df[replay_df.basepath == basepath]
 
-    starts = replay_df[
-        (replay_df.score_pval_time_swap <= alpha) & (replay_df.replay_type == "forward")
-    ].start
-    stops = replay_df[
-        (replay_df.score_pval_time_swap <= alpha) & (replay_df.replay_type == "forward")
-    ].stop
+    sig_replay_idx = replay_df.score_pval_time_swap <= alpha
+
+    starts = replay_df[sig_replay_idx & (replay_df.replay_type == "forward")].start
+    stops = replay_df[sig_replay_idx & (replay_df.replay_type == "forward")].stop
     forward_replay = nel.EpochArray(np.array([starts, stops]).T)
 
-    starts = replay_df[
-        (replay_df.score_pval_time_swap <= alpha) & (replay_df.replay_type == "reverse")
-    ].start
-    stops = replay_df[
-        (replay_df.score_pval_time_swap <= alpha) & (replay_df.replay_type == "reverse")
-    ].stop
+    starts = replay_df[sig_replay_idx & (replay_df.replay_type == "reverse")].start
+    stops = replay_df[sig_replay_idx & (replay_df.replay_type == "reverse")].stop
     reverse_replay = nel.EpochArray(np.array([starts, stops]).T)
+
+    non_sig_replay_idx = replay_df.score_pval_time_swap > alpha
+    starts = replay_df[non_sig_replay_idx].start
+    stops = replay_df[non_sig_replay_idx].stop
+    canidate_non_replay = nel.EpochArray(np.array([starts, stops]).T)
 
     # get session epochs
     epoch_df = loading.load_epoch(basepath)
     # compress back to back sleep epochs
     epoch_df = compress_repeated_epochs.main(epoch_df)
+    # get index of linear track relative to other (pre is negative, post is positive)
+    idx = epoch_df.environment == "linear"
+    if sum(idx) > 1:
+        duration = epoch_df.stopTime - epoch_df.startTime
+        idx = idx & (duration == max(duration[idx]))
+    beh_idx_rel_to_linear = np.arange(epoch_df.shape[0]) - np.where(idx)[0][0]
+
     # add session epochs into nelpy epoch array
     behavior_epochs = nel.EpochArray(
         [np.array([epoch_df.startTime, epoch_df.stopTime]).T]
@@ -90,8 +96,18 @@ def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05):
     # epoch array of all ripple epochs that are not replay
     ripple_outside_replay = ripple_epochs[~all_replay]
 
+    ripple_outside_replay = ripple_outside_replay.expand(partic_pad)
+    canidate_non_replay = canidate_non_replay.expand(partic_pad)
+    if not all_replay.isempty:
+        all_replay = all_replay.expand(partic_pad)
+    if not forward_replay.isempty:
+        forward_replay = forward_replay.expand(partic_pad)
+    if not reverse_replay.isempty:
+        reverse_replay = reverse_replay.expand(partic_pad)
+
     epoch = []
     epoch_i = []
+    epoch_rel_i = []
     replay_par = []
     ripple_par = []
     UID = []
@@ -101,6 +117,9 @@ def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05):
     replay_fr = []
     ripple_fr = []
     avg_fr = []
+    non_replay_par = []
+    forward_replay_par = []
+    reverse_replay_par = []
     # iterate through all behavioral epochs and get ripple and replay participation
     for beh_ep_i, beh_ep in enumerate(behavior_epochs):
 
@@ -108,23 +127,19 @@ def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05):
 
         # get avg firing rate over epoch
         avg_fr.append(current_st.n_events / beh_ep.length)
-        
-        # get ripple firing rate
-        # check if any spikes left in epoch
-        if sta_placecells[beh_ep][all_replay].data is None:
-            replay_fr.append(current_st.n_events * np.nan)
-        else:
-            replay_fr.append(
-                sta_placecells[beh_ep][all_replay].n_events / beh_ep.length
-            )
 
         # get ripple firing rate
-        if sta_placecells[beh_ep][ripple_outside_replay].data is None:
+        # check if any spikes left in epoch
+        if current_st[all_replay].isempty:
+            replay_fr.append(current_st.n_events * np.nan)
+        else:
+            replay_fr.append(current_st[all_replay].n_events / beh_ep.length)
+
+        # get ripple firing rate
+        if current_st[ripple_outside_replay].isempty:
             ripple_fr.append(current_st.n_events * np.nan)
         else:
-            ripple_fr.append(
-                sta_placecells[beh_ep][ripple_outside_replay].n_events / beh_ep.length
-            )
+            ripple_fr.append(current_st[ripple_outside_replay].n_events / beh_ep.length)
 
         # get replay participation
         replay_par.append(
@@ -144,11 +159,48 @@ def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05):
             ).mean(axis=1)
         )
 
+        # get canidate participation
+        non_replay_par.append(
+            functions.get_participation(
+                current_st.data,
+                canidate_non_replay[beh_ep].starts,
+                canidate_non_replay[beh_ep].stops,
+            ).mean(axis=1)
+        )
+
+        # get forward replay participation
+        if not forward_replay.isempty:
+            forward_replay_par.append(
+                functions.get_participation(
+                    current_st.data,
+                    forward_replay[beh_ep].starts,
+                    forward_replay[beh_ep].stops,
+                ).mean(axis=1)
+            )
+        else:
+            forward_replay_par.append(current_st.n_events * np.nan)
+
+        # get reverse replay participation
+        if not forward_replay.isempty:
+            reverse_replay_par.append(
+                functions.get_participation(
+                    current_st.data,
+                    reverse_replay[beh_ep].starts,
+                    reverse_replay[beh_ep].stops,
+                ).mean(axis=1)
+            )
+        else:
+            reverse_replay_par.append(current_st.n_events * np.nan)
+
         # get epoch info
         epoch.append(
             [epoch_df.environment.values[beh_ep_i]] * sta_placecells.data.shape[0]
         )
         epoch_i.append(np.tile(beh_ep_i, sta_placecells.data.shape[0]))
+        epoch_rel_i.append(
+            np.tile(beh_idx_rel_to_linear[beh_ep_i], sta_placecells.data.shape[0])
+        )
+
         # get UID
         UID.append(cell_metrics.UID.values)
         # get deep superficial distance
@@ -170,8 +222,12 @@ def run(basepath, replay_df=None, replay_save_path=None, alpha=0.05):
     temp_df["ripple_fr"] = np.hstack(ripple_fr)
     temp_df["replay_par"] = np.hstack(replay_par)
     temp_df["ripple_par"] = np.hstack(ripple_par)
+    temp_df["non_replay_par"] = np.hstack(non_replay_par)
+    temp_df["forward_replay_par"] = np.hstack(forward_replay_par)
+    temp_df["reverse_replay_par"] = np.hstack(reverse_replay_par)
     temp_df["epoch"] = np.hstack(epoch)
     temp_df["epoch_i"] = np.hstack(epoch_i)
+    temp_df["epoch_rel_i"] = np.hstack(epoch_rel_i)
     temp_df["UID"] = np.hstack(UID)
     temp_df["deepSuperficialDistance"] = np.hstack(deepSuperficialDistance)
     temp_df["n_replays"] = np.hstack(n_replays)
