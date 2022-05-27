4702669aaf4cd0ef5807032821b031829dba31ca
Ryan Harvey <ryanh412@gmail.com>
2022-05-27 10:36:47 -0400

    added middle shuffle

--- ripple_heterogeneity/replay/influence_of_layer_bias_on_replay.py
+++ ripple_heterogeneity/replay/influence_of_layer_bias_on_replay.py
@@ -122,14 +122,17 @@ def get_shuffled_labels(cell_metrics, replay_par_mat, ep, n_shuffles=1000):
     Output:
         n_deep: shuffled number of deep cells
         n_sup: shuffled number of superficial cells
+        n_middle: shuffled number of middle cells
     """
     n_deep = []
     n_sup = []
+    n_middle = []
     for _ in range(n_shuffles):
         labels = shuffle_labels(cell_metrics.deepSuperficial)
         n_deep.append(sum(labels[replay_par_mat[:, ep] == 1] == "Deep"))
         n_sup.append(sum(labels[replay_par_mat[:, ep] == 1] == "Superficial"))
-    return n_deep, n_sup
+        n_middle.append(sum(labels[replay_par_mat[:, ep] == 1] == "middle"))
+    return n_deep, n_sup, n_middle
 
 
 def get_weighted_avg_pyr_dist(cell_metrics, replay_par_mat):
@@ -230,23 +233,27 @@ def get_significant_events(cell_metrics, replay_par_mat, n_shuffles=1000, q_perc
     # initialize variables
     n_deep = np.zeros([n_shuffles, replay_par_mat.shape[1]])
     n_sup = np.zeros([n_shuffles, replay_par_mat.shape[1]])
+    n_middle = np.zeros([n_shuffles, replay_par_mat.shape[1]])
 
     # get the shuffled number of deep and superficial cells
     for i in range(replay_par_mat.shape[1]):
-        n_deep[:, i], n_sup[:, i] = get_shuffled_labels(
+        n_deep[:, i], n_sup[:, i], n_middle[:, i] = get_shuffled_labels(
             cell_metrics, replay_par_mat, i, n_shuffles=n_shuffles
         )
 
     # get the number of observed deep and superficial cells
     n_deep_obs = []
     n_sup_obs = []
+    n_middle_obs = []
     for i in range(replay_par_mat.shape[1]):
         cur_idx = replay_par_mat[:, i] == 1
         deep_idx = cell_metrics[cur_idx].deepSuperficial.values == "Deep"
         sup_idx = cell_metrics[cur_idx].deepSuperficial.values == "Superficial"
+        middle_idx = cell_metrics[cur_idx].deepSuperficial.values == "middle"
 
         n_deep_obs.append(sum(deep_idx))
         n_sup_obs.append(sum(sup_idx))
+        n_middle_obs.append(sum(middle_idx))
 
     # get the index of significant events
     sig_idx_deep, _ = functions.get_significant_events(
@@ -255,8 +262,18 @@ def get_significant_events(cell_metrics, replay_par_mat, n_shuffles=1000, q_perc
     sig_idx_sup, _ = functions.get_significant_events(
         np.hstack(n_sup_obs), n_sup, q=q_perc
     )
+    sig_idx_middle, _ = functions.get_significant_events(
+        np.hstack(n_middle_obs), n_middle, q=q_perc
+    )
 
-    return sig_idx_deep, sig_idx_sup, n_deep_obs, n_sup_obs
+    return (
+        sig_idx_deep,
+        sig_idx_sup,
+        sig_idx_middle,
+        n_deep_obs,
+        n_sup_obs,
+        n_middle_obs,
+    )
 
 
 def run(
@@ -292,7 +309,7 @@ def run(
         )
         replay_df = replay_df[df_idx]
     else:
-        df_idx = (replay_df.basepath == basepath)
+        df_idx = replay_df.basepath == basepath
         replay_df = replay_df[df_idx]
 
     # get the replay epochs for this basepath
@@ -314,7 +331,14 @@ def run(
     )
 
     # get the significant events
-    (sig_idx_deep, sig_idx_sup, n_deep_obs, n_sup_obs,) = get_significant_events(
+    (
+        sig_idx_deep,
+        sig_idx_sup,
+        sig_idx_middle,
+        n_deep_obs,
+        n_sup_obs,
+        n_middle_obs,
+    ) = get_significant_events(
         cell_metrics, replay_par_mat, n_shuffles=n_shuffles, q_perc=q_perc
     )
 
@@ -326,8 +350,10 @@ def run(
     temp_df["sig_unit_bias"] = "unknown"
     temp_df.loc[sig_idx_sup, "sig_unit_bias"] = "sup"
     temp_df.loc[sig_idx_deep, "sig_unit_bias"] = "deep"
+    temp_df.loc[sig_idx_middle, "sig_unit_bias"] = "middle"
     temp_df["n_deep_obs"] = n_deep_obs
     temp_df["n_sup_obs"] = n_sup_obs
+    temp_df["n_middle_obs"] = n_middle_obs
 
     temp_df = pd.concat([temp_df, weighted_df], axis=1)
 
