043c0d6b02604c3dd1355726b70ee585f8170be2
Ryan Harvey <ryanh412@gmail.com>
2022-07-15 10:31:13 -0400

    changed default params & added n cross region pairs
    extend ripple -50 and +200 ms and changed weight_dt to 100ms to capture downstream effect

--- ripple_heterogeneity/readout/assembly_multi_region.py
+++ ripple_heterogeneity/readout/assembly_multi_region.py
@@ -5,16 +5,18 @@ import numpy as np
 import pandas as pd
 from ripple_heterogeneity.assembly import assembly_reactivation, find_sig_assembly
 from ripple_heterogeneity.utils import add_new_deep_sup
-
+import warnings
 
 
 def run(
     basepath,
     regions="CA1|PFC|EC1|EC2|EC3|EC4|EC5|MEC",
-    putativeCellType="Pyr",
-    weight_dt=0.02,
-    verbose=False,
-    rip_expand=0.05,
+    putativeCellType="Pyr", 
+    weight_dt=0.1, # dt in seconds for binning st to get weights for each assembly
+    verbose=False, # print out progress
+    rip_expand=0.05, # expand the ripple region by this amount (not used)
+    rip_exp_start=0.05,  # ripple expansion start, in seconds, how much to expand ripples
+    rip_exp_stop=0.2,  # ripple expansion stop, in seconds, how much to expand ripples
 ):
     """
     Gets the pre and post assembly strengths
@@ -31,7 +33,8 @@ def run(
     m1.load_data()
 
     # extend ripples to include some extra time
-    m1.ripples = m1.ripples.expand(rip_expand)
+    m1.ripples = m1.ripples.expand(rip_exp_start, direction="start")
+    m1.ripples = m1.ripples.expand(rip_exp_stop, direction="stop")
 
     # check if no cells were found
     if m1.cell_metrics.shape[0] == 0:
@@ -103,11 +106,19 @@ def compile_results_df(results):
     deep_pfc = []
     superficial_mec = []
     superficial_pfc = []
-
+    n_deep = []
+    n_sup = []
+    n_mec = []
+    n_pfc = []
     for n in assembly_df.assembly_n.unique():
         temp_assembly_df = assembly_df[
             (assembly_df.assembly_n == n) & (assembly_df.is_member_sig)
         ]
+        n_deep.append(np.sum(temp_assembly_df.deepSuperficial == "Deep"))
+        n_sup.append(np.sum(temp_assembly_df.deepSuperficial == "Superficial"))
+        n_mec.append(np.sum(temp_assembly_df.deepSuperficial == "EC1|EC2|EC3|EC4|EC5|MEC"))
+        n_pfc.append(np.sum(temp_assembly_df.deepSuperficial == "PFC"))
+
         deep_mec.append(
             any(temp_assembly_df.brainRegion.str.contains("EC1|EC2|EC3|EC4|EC5|MEC"))
             & any((temp_assembly_df.deepSuperficial == "Deep"))
@@ -132,6 +143,14 @@ def compile_results_df(results):
         np.mean(np.array(deep_pfc) > 0),
         np.mean(np.array(deep_mec) > 0),
     ]
+
+    prop_df["n_cross_region"] = [
+        np.sum(np.array(superficial_pfc) > 0),
+        np.sum(np.array(superficial_mec) > 0),
+        np.sum(np.array(deep_pfc) > 0),
+        np.sum(np.array(deep_mec) > 0),
+    ]
+    
     prop_df["labels"] = ["Superficial PFC", "Superficial MEC", "Deep PFC", "Deep MEC"]
 
     results.get(
@@ -162,7 +181,7 @@ def load_results(save_path, verbose=False):
     """
     load_results: load results from a directory
     """
-
+    warnings.filterwarnings("ignore")
     print("Loading results...")
     sessions = glob.glob(save_path + os.sep + "*.pkl")
     prop_df = pd.DataFrame()
