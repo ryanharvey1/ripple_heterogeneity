c3329c2bab1a473c261813e52b75612df2a33a8f
Ryan Harvey <ryanh412@gmail.com>
2022-07-17 15:49:17 -0400

    clean up and docs

--- ripple_heterogeneity/readout/assembly_multi_region.py
+++ ripple_heterogeneity/readout/assembly_multi_region.py
@@ -180,6 +180,10 @@ def compile_results_df(results):
 
 
 def load_reactivation(results):
+    """
+    Loads reactivation data from results in pickle file.
+    """
+
     def get_strength_matrix(analog_signal):
         """
         Returns a matrix of the strength of the signal at each timepoint.
@@ -212,6 +216,9 @@ def load_reactivation(results):
         return df_strength
 
     def get_assembly_df(results, is_member):
+        """
+        Returns a dataframe of the assembly properties.
+        """
         assembly_df = pd.DataFrame()
 
         patterns = results.get("react").patterns
@@ -245,6 +252,9 @@ def load_reactivation(results):
         return assembly_df
 
     def get_assembly_cross_members(assembly_df):
+        """
+        Returns lists of the number of assemblies that are cross-members of each brain region.
+        """
         deep_mec = []
         deep_pfc = []
         superficial_mec = []
@@ -320,9 +330,10 @@ def load_reactivation(results):
 
     return df_strength
 
+
 def load_results(save_path, verbose=False):
     """
-    load_results: load results from a directory
+    load_results: load results from a pickle file
     """
     warnings.filterwarnings("ignore")
     print("Loading results...")
@@ -347,93 +358,5 @@ def load_results(save_path, verbose=False):
         prop_df = pd.concat([prop_df, prop_df_], ignore_index=True)
         assembly_df = pd.concat([assembly_df, assembly_df_], ignore_index=True)
         df_strength = pd.concat([df_strength, df_strength_], ignore_index=True)
-        
+
     return prop_df, assembly_df, df_strength
-
-
-# def run(basepath, regions="CA1|PFC|EC1|EC2|EC3|EC4|EC5|MEC", putativeCellType="Pyr"):
-
-#     ar = assembly_reactivation.AssemblyReact(
-#         basepath, brainRegion=regions, putativeCellType=putativeCellType
-#     )
-#     # use built in function to load needed data
-#     ar.load_data()
-#     # retain pre task post task structure
-#     ar.restrict_epochs_to_pre_task_post()
-#     # get weights for the task
-#     ar.get_weights([ar.epochs[1]])
-#     # locate sig assemblies and sig members
-#     patterns, is_member_sig, keep_assembly, is_member = find_sig_assembly.main(
-#         ar.patterns
-#     )
-
-#     assembly_df = pd.DataFrame()
-#     assembly_df["patterns"] = patterns.ravel()
-#     assembly_df["is_member_sig"] = is_member_sig.ravel()
-#     assembly_df["assembly_n"] = (
-#         (np.ones_like(patterns).T * np.arange(patterns.shape[0])).T.astype(int).ravel()
-#     )
-#     assembly_df["UID"] = np.tile(ar.cell_metrics.UID.values, patterns.shape[0])
-#     assembly_df["putativeCellType"] = np.tile(
-#         ar.cell_metrics.putativeCellType.values, patterns.shape[0]
-#     )
-#     assembly_df["brainRegion"] = np.tile(
-#         ar.cell_metrics.brainRegion.values, patterns.shape[0]
-#     )
-#     assembly_df["deepSuperficial"] = np.tile(
-#         ar.cell_metrics.deepSuperficial.values, patterns.shape[0]
-#     )
-#     assembly_df["deepSuperficialDistance"] = np.tile(
-#         ar.cell_metrics.deepSuperficialDistance.values, patterns.shape[0]
-#     )
-
-
-# deep_mec = []
-# deep_pfc = []
-# superficial_mec = []
-# superficial_pfc = []
-
-# for n in assembly_df.assembly_n.unique():
-#     temp_assembly_df = assembly_df[
-#         (assembly_df.assembly_n == n) & (assembly_df.is_member_sig)
-#     ]
-#     deep_mec.append(
-#         any(temp_assembly_df.brainRegion.str.contains("EC1|EC2|EC3|EC4|EC5|MEC"))
-#         & any((temp_assembly_df.deepSuperficial == "Deep"))
-#     )
-#     deep_pfc.append(
-#         any(temp_assembly_df.brainRegion.str.contains("PFC"))
-#         & any((temp_assembly_df.deepSuperficial == "Deep"))
-#     )
-#     superficial_mec.append(
-#         any(temp_assembly_df.brainRegion.str.contains("EC1|EC2|EC3|EC4|EC5|MEC"))
-#         & any((temp_assembly_df.deepSuperficial == "Superficial"))
-#     )
-#     superficial_pfc.append(
-#         any(temp_assembly_df.brainRegion.str.contains("PFC"))
-#         & any((temp_assembly_df.deepSuperficial == "Superficial"))
-#     )
-
-
-# prop_df = pd.DataFrame()
-# prop_df["prop_cross_region"] = [
-#     np.mean(np.array(superficial_pfc) > 0),
-#     np.mean(np.array(superficial_mec) > 0),
-#     np.mean(np.array(deep_pfc) > 0),
-#     np.mean(np.array(deep_mec) > 0),
-# ]
-# prop_df["labels"] = ["Superficial PFC", "Superficial MEC", "Deep PFC", "Deep MEC"]
-# prop_df["n_assemblies"] = len(superficial_pfc)
-# prop_df['basepath'] = basepath
-
-# results = {"assembly_df": assembly_df, "prop_df": prop_df}
-# return results
-
-# target_regions = ["PFC","EC1|EC2|EC3|EC4|EC5|MEC"]
-# for ca1_sub in ["Deep", "Superficial"]:
-#     # iterate over target regions
-#     for region in target_regions:
-#         for n in assembly_df.assembly_n.unique():
-#             temp_assembly_df = assembly_df[
-#                 (assembly_df.assembly_n == n) & (assembly_df.is_member_sig)
-#             ]
