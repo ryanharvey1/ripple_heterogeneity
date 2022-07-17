595e80e295bf5e09fb545fa6f48275b87c44771e
Ryan Harvey <ryanh412@gmail.com>
2022-07-17 15:29:05 -0400

    load_reactivation

--- ripple_heterogeneity/readout/assembly_multi_region.py
+++ ripple_heterogeneity/readout/assembly_multi_region.py
@@ -11,10 +11,10 @@ import warnings
 def run(
     basepath,
     regions="CA1|PFC|EC1|EC2|EC3|EC4|EC5|MEC",
-    putativeCellType="Pyr", 
-    weight_dt=0.1, # dt in seconds for binning st to get weights for each assembly
-    verbose=False, # print out progress
-    rip_expand=0.05, # expand the ripple region by this amount (not used)
+    putativeCellType="Pyr",
+    weight_dt=0.1,  # dt in seconds for binning st to get weights for each assembly
+    verbose=False,  # print out progress
+    rip_expand=0.05,  # expand the ripple region by this amount (not used)
     rip_exp_start=0.05,  # ripple expansion start, in seconds, how much to expand ripples
     rip_exp_stop=0.2,  # ripple expansion stop, in seconds, how much to expand ripples
 ):
@@ -116,7 +116,9 @@ def compile_results_df(results):
         ]
         n_deep.append(np.sum(temp_assembly_df.deepSuperficial == "Deep"))
         n_sup.append(np.sum(temp_assembly_df.deepSuperficial == "Superficial"))
-        n_mec.append(np.sum(temp_assembly_df.deepSuperficial == "EC1|EC2|EC3|EC4|EC5|MEC"))
+        n_mec.append(
+            np.sum(temp_assembly_df.deepSuperficial == "EC1|EC2|EC3|EC4|EC5|MEC")
+        )
         n_pfc.append(np.sum(temp_assembly_df.deepSuperficial == "PFC"))
 
         deep_mec.append(
@@ -150,7 +152,7 @@ def compile_results_df(results):
         np.sum(np.array(deep_pfc) > 0),
         np.sum(np.array(deep_mec) > 0),
     ]
-    
+
     prop_df["labels"] = ["Superficial PFC", "Superficial MEC", "Deep PFC", "Deep MEC"]
 
     results.get(
@@ -177,15 +179,160 @@ def compile_results_df(results):
     return prop_df, assembly_df
 
 
+def load_reactivation(results):
+    def get_strength_matrix(analog_signal):
+        """
+        Returns a matrix of the strength of the signal at each timepoint.
+        input:
+            analog_signal: a nelpy AnalogSignalArray of the assembly strengths, must have segments
+        output:
+            strength_matrix: a numpy array of the strength of the signal at each timepoint
+        """
+        strength = np.zeros([analog_signal.n_signals, analog_signal.n_intervals])
+
+        for rip_i, rip in enumerate(analog_signal):
+            strength[:, rip_i] = rip.max()
+        return strength
+
+    def compile_strength_df(strength, keep_assembly):
+        """
+        Compiles a dataframe of the strength of the signal at each timepoint.
+        input:
+            strength: a numpy array of the strength of the signal at each timepoint
+            keep_assembly: a list of assemblies to keep marked as significant
+        output:
+            strength_df: a dataframe of the strength of the signal at each timepoint
+        """
+        df_strength = pd.DataFrame()
+        df_strength["strength"] = strength.flatten()
+        df_strength["assembly_n"] = (
+            np.ones_like(strength).T * np.arange(strength.shape[0])
+        ).T.flatten()
+        df_strength["sig"] = (np.ones_like(strength).T * keep_assembly).T.flatten()
+        return df_strength
+
+    def get_assembly_df(results, is_member):
+        assembly_df = pd.DataFrame()
+
+        patterns = results.get("react").patterns
+
+        assembly_df["patterns"] = patterns.ravel()
+        assembly_df["is_member"] = is_member.ravel()
+        assembly_df["assembly_n"] = (
+            (np.ones_like(patterns).T * np.arange(patterns.shape[0]))
+            .T.astype(int)
+            .ravel()
+        )
+        assembly_df["UID"] = np.tile(
+            results.get("react").cell_metrics.UID.values, patterns.shape[0]
+        )
+        assembly_df["putativeCellType"] = np.tile(
+            results.get("react").cell_metrics.putativeCellType.values, patterns.shape[0]
+        )
+        assembly_df["brainRegion"] = np.tile(
+            results.get("react").cell_metrics.brainRegion.values, patterns.shape[0]
+        )
+        assembly_df["deepSuperficial"] = np.tile(
+            results.get("react").cell_metrics.deepSuperficial.values, patterns.shape[0]
+        )
+        assembly_df["deepSuperficialDistance"] = np.tile(
+            results.get("react").cell_metrics.deepSuperficialDistance.values,
+            patterns.shape[0],
+        )
+        assembly_df = add_new_deep_sup.deep_sup_from_deepSuperficialDistance(
+            assembly_df
+        )
+        return assembly_df
+
+    def get_assembly_cross_members(assembly_df):
+        deep_mec = []
+        deep_pfc = []
+        superficial_mec = []
+        superficial_pfc = []
+        for n in assembly_df.assembly_n.unique():
+            temp_assembly_df = assembly_df[
+                (assembly_df.assembly_n == n) & (assembly_df.is_member)
+            ]
+            deep_mec.append(
+                any(
+                    temp_assembly_df.brainRegion.str.contains("EC1|EC2|EC3|EC4|EC5|MEC")
+                )
+                & any((temp_assembly_df.deepSuperficial == "Deep"))
+            )
+            deep_pfc.append(
+                any(temp_assembly_df.brainRegion.str.contains("PFC"))
+                & any((temp_assembly_df.deepSuperficial == "Deep"))
+            )
+            superficial_mec.append(
+                any(
+                    temp_assembly_df.brainRegion.str.contains("EC1|EC2|EC3|EC4|EC5|MEC")
+                )
+                & any((temp_assembly_df.deepSuperficial == "Superficial"))
+            )
+            superficial_pfc.append(
+                any(temp_assembly_df.brainRegion.str.contains("PFC"))
+                & any((temp_assembly_df.deepSuperficial == "Superficial"))
+            )
+        return deep_mec, deep_pfc, superficial_mec, superficial_pfc
+
+    patterns, is_member_sig, keep_assembly, is_member = find_sig_assembly.main(
+        results.get("react").patterns
+    )
+
+    strength = get_strength_matrix(results["assembly_act_pre"])
+    df_strength_pre = compile_strength_df(strength, keep_assembly)
+    df_strength_pre["epoch"] = "pre"
+
+    strength = get_strength_matrix(results["assembly_act_task"])
+    df_strength_task = compile_strength_df(strength, keep_assembly)
+    df_strength_task["epoch"] = "task"
+
+    strength = get_strength_matrix(results["assembly_act_post"])
+    df_strength_post = compile_strength_df(strength, keep_assembly)
+    df_strength_post["epoch"] = "post"
+
+    df_strength = pd.DataFrame()
+    df_strength = pd.concat(
+        [df_strength_pre, df_strength_task, df_strength_post], ignore_index=True
+    )
+
+    assembly_df = get_assembly_df(results, is_member)
+    deep_mec, deep_pfc, superficial_mec, superficial_pfc = get_assembly_cross_members(
+        assembly_df
+    )
+
+    labels = ["deep_mec", "deep_pfc", "superficial_mec", "superficial_pfc"]
+    for label in labels:
+        df_strength[label] = np.zeros_like(df_strength.assembly_n)
+
+    df_strength.loc[
+        np.in1d(df_strength.assembly_n, np.where(deep_mec)[0]), "deep_mec"
+    ] = 1
+    df_strength.loc[
+        np.in1d(df_strength.assembly_n, np.where(deep_pfc)[0]), "deep_pfc"
+    ] = 1
+    df_strength.loc[
+        np.in1d(df_strength.assembly_n, np.where(superficial_mec)[0]), "superficial_mec"
+    ] = 1
+    df_strength.loc[
+        np.in1d(df_strength.assembly_n, np.where(superficial_pfc)[0]), "superficial_pfc"
+    ] = 1
+
+    return df_strength
+
 def load_results(save_path, verbose=False):
     """
     load_results: load results from a directory
     """
     warnings.filterwarnings("ignore")
     print("Loading results...")
+
     sessions = glob.glob(save_path + os.sep + "*.pkl")
+
     prop_df = pd.DataFrame()
     assembly_df = pd.DataFrame()
+    df_strength = pd.DataFrame()
+
     for session in sessions:
         if verbose:
             print(session)
@@ -195,12 +342,13 @@ def load_results(save_path, verbose=False):
             continue
 
         prop_df_, assembly_df_ = compile_results_df(results)
+        df_strength_ = load_reactivation(results)
 
         prop_df = pd.concat([prop_df, prop_df_], ignore_index=True)
-        assembly_df = pd.concat(
-            [assembly_df, assembly_df_], ignore_index=True
-        )
-    return prop_df, assembly_df
+        assembly_df = pd.concat([assembly_df, assembly_df_], ignore_index=True)
+        df_strength = pd.concat([df_strength, df_strength_], ignore_index=True)
+        
+    return prop_df, assembly_df, df_strength
 
 
 # def run(basepath, regions="CA1|PFC|EC1|EC2|EC3|EC4|EC5|MEC", putativeCellType="Pyr"):
