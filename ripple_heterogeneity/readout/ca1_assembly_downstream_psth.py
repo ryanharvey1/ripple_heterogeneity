b34217a6a0646752679ddcec04e595d219eb36ff
Ryan Harvey <ryanh412@gmail.com>
2022-07-20 19:12:59 -0400

    built out analysis

--- ripple_heterogeneity/readout/ca1_assembly_downstream_psth.py
+++ ripple_heterogeneity/readout/ca1_assembly_downstream_psth.py
@@ -5,8 +5,114 @@ import numpy as np
 import pandas as pd
 from ripple_heterogeneity.assembly import assembly_reactivation, find_sig_assembly
 from ripple_heterogeneity.utils import add_new_deep_sup
-import warnings
 from ripple_heterogeneity.utils import functions, loading, add_new_deep_sup
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def get_assembly_df(results, is_member):
+    """
+    Returns a dataframe of the assembly properties.
+    """
+    assembly_df = pd.DataFrame()
+
+    patterns = results.get("react").patterns
+
+    assembly_df["patterns"] = patterns.ravel()
+    assembly_df["is_member"] = is_member.ravel()
+    assembly_df["assembly_n"] = (
+        (np.ones_like(patterns).T * np.arange(patterns.shape[0])).T.astype(int).ravel()
+    )
+    assembly_df["UID"] = np.tile(
+        results.get("react").cell_metrics.UID.values, patterns.shape[0]
+    )
+    assembly_df["putativeCellType"] = np.tile(
+        results.get("react").cell_metrics.putativeCellType.values, patterns.shape[0]
+    )
+    assembly_df["brainRegion"] = np.tile(
+        results.get("react").cell_metrics.brainRegion.values, patterns.shape[0]
+    )
+    assembly_df["deepSuperficial"] = np.tile(
+        results.get("react").cell_metrics.deepSuperficial.values, patterns.shape[0]
+    )
+    assembly_df["deepSuperficialDistance"] = np.tile(
+        results.get("react").cell_metrics.deepSuperficialDistance.values,
+        patterns.shape[0],
+    )
+    assembly_df = add_new_deep_sup.deep_sup_from_deepSuperficialDistance(assembly_df)
+    return assembly_df
+
+
+def get_strength_matrix(analog_signal):
+    """
+    Returns a matrix of the strength of the signal at each timepoint.
+    input:
+        analog_signal: a nelpy AnalogSignalArray of the assembly strengths, must have segments
+    output:
+        strength_matrix: a numpy array of the strength of the signal at each timepoint
+    """
+    strength = np.zeros([analog_signal.n_signals, analog_signal.n_intervals])
+    time = strength.copy()
+    for rip_i, rip in enumerate(analog_signal):
+        strength[:, rip_i] = rip.max()
+        time[:, rip_i] = rip.abscissa_vals[np.argmax(rip.data, axis=1)]
+    return strength, time
+
+
+def get_psths(
+    st,
+    assembly_strengths,
+    assembly_df,
+    labels=["Deep", "Superficial", "mixed"],
+    assembly_strength_thres=5,
+    bin_width=0.005,
+    n_bins=200,
+):
+    """
+    Returns a dict of the PSTH of st relative to when the assembly is active.
+    input:
+        st: a nelpy SpikeTrainArray of the st of the assembly
+        assembly_strengths: a nelpy analogsignal array of the strength of the signal at each ripple
+        assembly_df: a dataframe of the assembly properties
+        labels: a list of the labels of the assemblies
+        assembly_strength_thres: the threshold for the strength of the assembly
+        bin_width: the width of the bins in seconds
+        n_bins: the number of bins
+    output:
+        psths: a dict of the PSTH of st relative to when the assembly is active
+    """
+    psths = {}
+    for label in labels:
+        psth = pd.DataFrame()
+        # get the max value and time of max value for each assembly over ripples
+        strength, time = get_strength_matrix(assembly_strengths)
+
+        # keep the assemblies that are specific to the label
+        assembly_n = assembly_df[
+            assembly_df.assembly_label == label
+        ].assembly_n.unique()
+
+        strength = strength[assembly_n, :]
+        time = time[assembly_n, :]
+
+        # iterate over the assemblies and calculate the psth relative to the time of max strength
+        for i in range(strength.shape[0]):
+            psth = pd.concat(
+                [
+                    psth,
+                    functions.compute_psth(
+                        st.data,
+                        time[i, (strength[i, :] > assembly_strength_thres)],
+                        bin_width=bin_width,
+                        n_bins=n_bins,
+                    ),
+                ],
+                ignore_index=True,
+            )
+        # add the psth to the dict
+        psths[label] = psth
+    return psths
 
 
 def run(
@@ -74,3 +180,46 @@ def run(
     st, cell_metrics = loading.load_spikes(
         basepath, brainRegion=target_regions, putativeCellType=putativeCellType
     )
+
+    patterns, is_member_sig, keep_assembly, is_member = find_sig_assembly.main(
+        results.get("react").patterns
+    )
+    assembly_df = get_assembly_df(results, is_member)
+    # assembly_df
+
+    for assembly_n in assembly_df.assembly_n.unique():
+        assembly_n_idx = assembly_df.assembly_n == assembly_n
+        if (assembly_df[assembly_n_idx].deepSuperficial == "Deep").all():
+            assembly_df.loc[assembly_n_idx, "assembly_label"] = "Deep"
+        elif (assembly_df[assembly_n_idx].deepSuperficial == "Superficial").all():
+            assembly_df.loc[assembly_n_idx, "assembly_label"] = "Superficial"
+        else:
+            assembly_df.loc[assembly_n_idx, "assembly_label"] = "mixed"
+
+    result = {}
+    result["pre"] = get_psths(
+        st,
+        results.get("assembly_act_pre"),
+        assembly_df
+    )
+
+    result["task"] = get_psths(
+        st,
+        results.get("assembly_act_task"),
+        assembly_df
+    )
+
+    result["post"] = get_psths(
+        st,
+        results.get("assembly_act_post"),
+        assembly_df
+    )
+    return result
+
+def load_results():
+    """
+    Loads the results from the previous run
+    """
+    print("Loading results...")
+    print("not implemented yet")
+    pass
