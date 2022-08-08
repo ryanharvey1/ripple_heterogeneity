57bf402b6d79ba0fc2fa65e8a80f434ebd75c9b2
Ryan Harvey <ryanh412@gmail.com>
2022-08-07 21:21:10 -0400

    working edits

--- ripple_heterogeneity/utils/linearization_pipeline.py
+++ ripple_heterogeneity/utils/linearization_pipeline.py
@@ -1,15 +1,20 @@
+import glob
+import os
+import sys
 from ripple_heterogeneity.utils import loading
 import numpy as np
 import matplotlib.pyplot as plt
 from track_linearization import make_track_graph
 from track_linearization import get_linearized_position
+from scipy.io import savemat, loadmat
 
 plt.ion()
 
-class NodePicker():
+
+class NodePicker:
     """Interactive creation of track graph by looking at video frames."""
 
-    def __init__(self, ax=None, video_filename=None, node_color="r", node_size=100):
+    def __init__(self, ax=None, basepath=None, node_color="r", node_size=100):
         if ax is None:
             ax = plt.gca()
         self.ax = ax
@@ -17,14 +22,14 @@ class NodePicker():
         self.cid = None
         self._nodes = []
         self.node_color = node_color
-        self._nodes_plot = ax.scatter(
-            [], [],
-            zorder=5, s=node_size, color=node_color)
+        self._nodes_plot = ax.scatter([], [], zorder=5, s=node_size, color=node_color)
         self.edges = [[]]
-        self.video_filename = video_filename
+        self.basepath = basepath
 
-        ax.set_title('Left click to place node.\nRight click to remove node.'
-                        '\nShift+Left click to clear nodes.\nCntrl+Left click two nodes to place an edge')
+        ax.set_title(
+            "Left click to place node.\nRight click to remove node."
+            "\nShift+Left click to clear nodes.\nCntrl+Left click two nodes to place an edge"
+        )
         self.canvas.draw()
 
         self.connect()
@@ -35,8 +40,7 @@ class NodePicker():
 
     def connect(self):
         if self.cid is None:
-            self.cid = self.canvas.mpl_connect(
-                'button_press_event', self.click_event)
+            self.cid = self.canvas.mpl_connect("button_press_event", self.click_event)
 
     def disconnect(self):
         if self.cid is not None:
@@ -46,24 +50,25 @@ class NodePicker():
     def click_event(self, event):
         if not event.inaxes:
             return
-        if (event.key not in ['control', 'shift']) & (event.button == 1):  # left click
+        if (event.key not in ["control", "shift"]) & (event.button == 1):  # left click
             self._nodes.append((event.xdata, event.ydata))
-        if (event.key not in ['control', 'shift']) & (event.button == 3):  # right click
+        if (event.key not in ["control", "shift"]) & (event.button == 3):  # right click
             self.remove_point((event.xdata, event.ydata))
-        if (event.key == 'shift') & (event.button == 1):
+        if (event.key == "shift") & (event.button == 1):
             self.clear()
-        if (event.key == 'control') & (event.button == 1):
+        if (event.key == "control") & (event.button == 1):
             point = (event.xdata, event.ydata)
-            distance_to_nodes = np.linalg.norm(
-                self.node_positions - point, axis=1)
+            distance_to_nodes = np.linalg.norm(self.node_positions - point, axis=1)
             closest_node_ind = np.argmin(distance_to_nodes)
             if len(self.edges[-1]) < 2:
                 self.edges[-1].append(closest_node_ind)
             else:
                 self.edges.append([closest_node_ind])
             # print(self.edges)
-        if (event.key == 'enter'):
-            self.disconnect()
+        # print(event.key)
+        if event.key == "enter":
+            # self.disconnect()
+            self.format_and_save()
 
         self.redraw()
 
@@ -77,32 +82,34 @@ class NodePicker():
         # Draw Node Numbers
         # self.ax.texts = []
         for ind, (x, y) in enumerate(self.node_positions):
-            self.ax.text(x, y, ind,
-                         zorder=6,
-                         fontsize=12,
-                         horizontalalignment='center',
-                         verticalalignment='center',
-                         clip_on=True,
-                         bbox=None,
-                         transform=self.ax.transData,
-                         )
+            self.ax.text(
+                x,
+                y,
+                ind,
+                zorder=6,
+                fontsize=12,
+                horizontalalignment="center",
+                verticalalignment="center",
+                clip_on=True,
+                bbox=None,
+                transform=self.ax.transData,
+            )
         # Draw Edges
         # self.ax.lines = []  # clears the existing lines
         for edge in self.edges:
             if len(edge) > 1:
                 x1, y1 = self.node_positions[edge[0]]
                 x2, y2 = self.node_positions[edge[1]]
-                self.ax.plot([x1, x2], [y1, y2],
-                            color=self.node_color,
-                            linewidth=5, zorder=1000)
+                self.ax.plot(
+                    [x1, x2], [y1, y2], color=self.node_color, linewidth=5, zorder=1000
+                )
 
-        self.canvas.draw_idle()
-        # self.canvas.draw()
+        # self.canvas.draw_idle()
+        self.canvas.draw()
 
     def remove_point(self, point):
         if len(self._nodes) > 0:
-            distance_to_nodes = np.linalg.norm(
-                self.node_positions - point, axis=1)
+            distance_to_nodes = np.linalg.norm(self.node_positions - point, axis=1)
             closest_node_ind = np.argmin(distance_to_nodes)
             self._nodes.pop(closest_node_ind)
 
@@ -111,30 +118,94 @@ class NodePicker():
         self.edges = [[]]
         self.redraw()
 
+    def format_and_save(self):
+
+        behave_df = loading.load_animal_behavior(self.basepath)
+        na_idx = np.isnan(behave_df.x)
+
+        track_graph = make_track_graph(self.node_positions, self.edges)
+
+        position = np.vstack(
+            [behave_df[~na_idx].x.values, behave_df[~na_idx].y.values]
+        ).T
+        position_df = get_linearized_position(
+            position=position,
+            track_graph=track_graph,
+            edge_order=self.edges,
+            use_HMM=True,
+        )
+
+        behave_df.loc[~na_idx, "linear_position"] = position_df.linear_position.values
+        behave_df.loc[~na_idx, "track_segment_id"] = position_df.track_segment_id.values
+        behave_df.loc[
+            ~na_idx, "projected_x_position"
+        ] = position_df.projected_x_position.values
+        behave_df.loc[
+            ~na_idx, "projected_y_position"
+        ] = position_df.projected_y_position.values
+
+        filename = glob.glob(os.path.join(self.basepath, "*.animal.behavior.mat"))[0]
+        data = loadmat(filename)
+
+        data["behavior"]["position"][0][0]["linearized"][0][0] = [behave_df.linear_position.values]
+        data["behavior"]["states"][0][0][0] = behave_df.track_segment_id.values
+        data["behavior"]["position"][0][0]["projected_x"][0][0] = [behave_df.projected_x_position.values]
+        data["behavior"]["position"][0][0]["projected_y"][0][0] = [behave_df.projected_y_position.values]
+
+        savemat(filename, data)
+
+
 def run(basepath):
-# video_filename = r"Z:\Data\ORproject\OR15\day1"
+    print("here is the file,", basepath)
     fig, ax = plt.subplots(figsize=(5, 5))
 
     behave_df = loading.load_animal_behavior(basepath)
-    nan_idx = np.isnan(behave_df[["x","y"]]).any(axis=1)
 
-    ax.scatter(behave_df.x, behave_df.y,color="grey",s=1)
+    ax.scatter(behave_df.x, behave_df.y, color="grey", s=1)
 
-    picker = NodePicker(ax=ax, video_filename=basepath)
-
-    track_graph = make_track_graph(picker.node_positions, picker.edges)
+    picker = NodePicker(ax=ax, basepath=basepath)
+    plt.show(block=True)
 
 
-    position = np.vstack([behave_df.x.values, behave_df.y.values])[:,~nan_idx].T
-    position_df = get_linearized_position(
-        position=position,
-        track_graph=track_graph,
-        edge_order=picker.edges,
-    )
+if __name__ == "__main__":
+    # fig.show()
+    run(sys.argv[1])
+    # while not picker.finished:
+    #     pass
 
-    behave_df.loc[~nan_idx, "linear_position"] = position_df.linear_position.values
-    behave_df.loc[~nan_idx, "track_segment_id"] = position_df.track_segment_id.values
-    behave_df.loc[~nan_idx, "projected_x_position"] = position_df.projected_x_position.values
-    behave_df.loc[~nan_idx, "projected_y_position"] = position_df.projected_y_position.values
-    
-    return behave_df
+    # track_graph = make_track_graph(picker.node_positions, picker.edges)
+
+    # position = np.vstack(behave_df[["x", "y"]].values)[:, ~na_idx].T
+    # position_df = get_linearized_position(
+    #     position=position,
+    #     track_graph=track_graph,
+    #     edge_order=picker.edges,
+    #     use_HMM=True,
+    # )
+
+    # behave_df.loc[~na_idx, "linear_position"] = position_df.linear_position.values
+    # behave_df.loc[~na_idx, "track_segment_id"] = position_df.track_segment_id.values
+    # behave_df.loc[
+    #     ~na_idx, "projected_x_position"
+    # ] = position_df.projected_x_position.values
+    # behave_df.loc[
+    #     ~na_idx, "projected_y_position"
+    # ] = position_df.projected_y_position.values
+
+    # filename = glob.glob(os.path.join(basepath, "*.animal.behavior.mat"))[0]
+    # data = loadmat(filename)
+
+    # data["behavior"]["position"][0][0]["linearized"][0][0][
+    #     0
+    # ] = behave_df.linear_position.values
+    # data["behavior"]["states"][0][0][0] = behave_df.track_segment_id.values
+    # data["behavior"]["position"][0][0]["projected_x"][0][0][
+    #     0
+    # ] = behave_df.projected_x_position.values
+    # data["behavior"]["position"][0][0]["projected_y"][0][0][
+    #     0
+    # ] = behave_df.projected_y_position.values
+
+    # savemat(filename, data)
+
+    # return behave_df
