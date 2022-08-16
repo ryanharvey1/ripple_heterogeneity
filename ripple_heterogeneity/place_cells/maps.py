9ec0889565e36a0fb11bc39d8611efe452c76403
Ryan Harvey <ryanh412@gmail.com>
2022-08-16 11:48:13 -0400

    adding docs and input params

--- ripple_heterogeneity/place_cells/maps.py
+++ ripple_heterogeneity/place_cells/maps.py
@@ -3,6 +3,24 @@ import nelpy as nel
 
 
 class SpatialMap(object):
+    """
+    SpatialMap: make a spatial map tuning curve
+    args:
+        pos: position data (nelpy.AnalogSignal)
+        st: spike train data (nelpy.SpikeTrain)
+        dim: dimension of the map (1 or 2)
+        dir_epoch: epochs of the running direction, for linear data (nelpy.Epoch)
+        speed_thres: speed threshold for running (float)
+        ds_bst: bin size for the spike train (float)
+        s_binsize: bin size for the spatial map (float)
+        tuning_curve_sigma: sigma for the tuning curve (float)
+        min_duration: minimum duration for a tuning curve (float)
+        minbgrate: min firing rate for tuning curve, will set to this if lower (float)
+    attributes:
+        tc: tuning curves (nelpy.TuningCurve)
+        st_run: spike train restricted to running epochs (nelpy.SpikeTrain)
+        bst_run: binned spike train restricted to running epochs (nelpy.binnedSpikeTrain)
+    """
     def __init__(
         self,
         pos,
@@ -13,6 +31,8 @@ class SpatialMap(object):
         ds_bst=0.05,
         s_binsize=3,
         tuning_curve_sigma=3,
+        min_duration=0.1,
+        minbgrate=0,
     ):
         self.pos = pos
         self.st = st
@@ -22,6 +42,8 @@ class SpatialMap(object):
         self.ds_bst = ds_bst
         self.s_binsize = s_binsize
         self.tuning_curve_sigma = tuning_curve_sigma
+        self.min_duration = min_duration
+        self.minbgrate = minbgrate
         self.speed = nel.utils.ddt_asa(self.pos, smooth=True, sigma=0.1, norm=True)
         self.run_epochs = nel.utils.get_run_epochs(
             self.speed, v1=self.speed_thres, v2=self.speed_thres
@@ -66,9 +88,10 @@ class SpatialMap(object):
         # restrict spike trains to those epochs during which the animal was running
         st_run = self.st[self.run_epochs]
 
-        # bin:
+        # bin spike trains
         bst_run = st_run.bin(ds=self.ds_bst)
 
+        # locate max and min of the position data
         ext_xmin, ext_xmax = (
             np.floor(self.pos[:, 0].min() / 10) * 10,
             np.ceil(self.pos[:, 0].max() / 10) * 10,
@@ -77,9 +100,11 @@ class SpatialMap(object):
             np.floor(self.pos[:, 1].min() / 10) * 10,
             np.ceil(self.pos[:, 1].max() / 10) * 10,
         )
+        # number of bins in each dimension
         ext_nx = int((ext_xmax - ext_xmin) / self.s_binsize)
         ext_ny = int((ext_ymax - ext_ymin) / self.s_binsize)
 
+        # construct the tuning curves
         tc = nel.TuningCurve2D(
             bst=bst_run,
             extern=self.pos[self.run_epochs],
@@ -90,7 +115,7 @@ class SpatialMap(object):
             ext_ny=ext_ny,
             ext_nx=ext_nx,
             sigma=self.tuning_curve_sigma,
-            min_duration=0.1,
-            minbgrate=0,
+            min_duration=self.min_duration,
+            minbgrate=self.minbgrate,
         )
         return tc, st_run, bst_run
