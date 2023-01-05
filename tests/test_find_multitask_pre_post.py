16fb2418e94c1b775c9f6f083c67149efdfc373e
Laura Berkowitz <leb239@cornell.edu>
2023-01-05 12:36:14 -0500

    added expected output

--- tests/test_find_multitask_pre_post.py
+++ tests/test_find_multitask_pre_post.py
@@ -10,11 +10,11 @@ df1 = pd.DataFrame({'name': ['pre_task_sleep', 'open_field', 'injection', 'post_
                    'environment': ['sleep', 'open_field', 'home_cage', 'sleep', 'open_field', 'sleep']})
 
 epoch_list = functions.find_multitask_pre_post(df1['environment'],task_tag = 'open_field')
-print(epoch_list)
+print(epoch_list) # [[0, 1, 3], [3, 4, 5]]
 
 # Example usage
 df2 = pd.DataFrame({'name': ['box', 'box', 'box', 'sleep', 'box', 'sleep','tmaze','sleep'],
                    'environment': ['box', 'box', 'box', 'sleep', 'box', 'sleep','tmaze','sleep']})
 
 epoch_list = functions.find_multitask_pre_post(df2['environment'],task_tag = 'box|tmaze')
-print(epoch_list)
+print(epoch_list) # [[3, 4, 5], [5, 6, 7]]
