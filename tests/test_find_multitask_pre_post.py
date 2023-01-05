22149f6f0533e9422352ab6208241d42393b6745
Laura Berkowitz <leb239@cornell.edu>
2023-01-05 12:35:09 -0500

    test code for new function

---
+++ tests/test_find_multitask_pre_post.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+from ripple_heterogeneity.utils import (
+    functions,
+)
+
+
+# Example usage
+df1 = pd.DataFrame({'name': ['pre_task_sleep', 'open_field', 'injection', 'post_task_sleep', 'test', 'post_task_sleep'],
+                   'environment': ['sleep', 'open_field', 'home_cage', 'sleep', 'open_field', 'sleep']})
+
+epoch_list = functions.find_multitask_pre_post(df1['environment'],task_tag = 'open_field')
+print(epoch_list)
+
+# Example usage
+df2 = pd.DataFrame({'name': ['box', 'box', 'box', 'sleep', 'box', 'sleep','tmaze','sleep'],
+                   'environment': ['box', 'box', 'box', 'sleep', 'box', 'sleep','tmaze','sleep']})
+
+epoch_list = functions.find_multitask_pre_post(df2['environment'],task_tag = 'box|tmaze')
+print(epoch_list)
