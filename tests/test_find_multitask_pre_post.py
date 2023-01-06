9f6de223fb477a5a9a9a5c2f3567e183acfa18bc
Ryan Harvey <ryanh412@gmail.com>
2023-01-06 11:01:18 -0500

    update test with assert

--- tests/test_find_multitask_pre_post.py
+++ tests/test_find_multitask_pre_post.py
@@ -1,20 +1,54 @@
-
 import pandas as pd
-from ripple_heterogeneity.utils import (
-    functions,
-)
+from ripple_heterogeneity.utils import functions
+import numpy as np
 
 
-# Example usage
-df1 = pd.DataFrame({'name': ['pre_task_sleep', 'open_field', 'injection', 'post_task_sleep', 'test', 'post_task_sleep'],
-                   'environment': ['sleep', 'open_field', 'home_cage', 'sleep', 'open_field', 'sleep']})
+def test_find_multitask_pre_post():
+    # Example usage
+    df1 = pd.DataFrame(
+        {
+            "name": [
+                "pre_task_sleep",
+                "open_field",
+                "injection",
+                "post_task_sleep",
+                "test",
+                "post_task_sleep",
+            ],
+            "environment": [
+                "sleep",
+                "open_field",
+                "home_cage",
+                "sleep",
+                "open_field",
+                "sleep",
+            ],
+        }
+    )
 
-epoch_list = functions.find_multitask_pre_post(df1['environment'],task_tag = 'open_field')
-print(epoch_list) # [[0, 1, 3], [3, 4, 5]]
+    epoch_list = functions.find_multitask_pre_post(
+        df1["environment"], task_tag="open_field"
+    )
+    assert np.allclose(epoch_list, [[0, 1, 3], [3, 4, 5]])
 
-# Example usage
-df2 = pd.DataFrame({'name': ['box', 'box', 'box', 'sleep', 'box', 'sleep','tmaze','sleep'],
-                   'environment': ['box', 'box', 'box', 'sleep', 'box', 'sleep','tmaze','sleep']})
+    # Example usage
+    df2 = pd.DataFrame(
+        {
+            "name": ["box", "box", "box", "sleep", "box", "sleep", "tmaze", "sleep"],
+            "environment": [
+                "box",
+                "box",
+                "box",
+                "sleep",
+                "box",
+                "sleep",
+                "tmaze",
+                "sleep",
+            ],
+        }
+    )
 
-epoch_list = functions.find_multitask_pre_post(df2['environment'],task_tag = 'box|tmaze')
-print(epoch_list) # [[3, 4, 5], [5, 6, 7]]
+    epoch_list = functions.find_multitask_pre_post(
+        df2["environment"], task_tag="box|tmaze"
+    )
+    assert np.allclose(epoch_list, [[3, 4, 5], [5, 6, 7]])
