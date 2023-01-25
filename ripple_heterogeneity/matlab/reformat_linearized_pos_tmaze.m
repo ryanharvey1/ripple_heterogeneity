3b20f1aa0401a0355fa25078c013b8d88121b1be
Ryan Harvey <ryanh412@gmail.com>
2023-01-25 15:04:13 -0500

    re write to process sub epochs

--- ripple_heterogeneity/matlab/reformat_linearized_pos_tmaze.m
+++ ripple_heterogeneity/matlab/reformat_linearized_pos_tmaze.m
@@ -2,16 +2,18 @@
 % df = df(contains(df.basepath,'OR'),:);
 % df.basepath = {'Z:\Data\FujisawaS\EE\EE0622fm'};
 % df = readtable('Z:\home\ryanh\projects\ripple_heterogeneity\tmaze_sessions.csv');
-opts = delimitedTextImportOptions("NumVariables", 1);
-opts.DataLines = [2, Inf];
-opts.Delimiter = ",";
-opts.VariableNames = "basepath";
-opts.VariableTypes = "string";
-opts.ExtraColumnsRule = "ignore";
-opts.EmptyLineRule = "read";
-opts = setvaropts(opts, "basepath", "WhitespaceRule", "preserve");
-opts = setvaropts(opts, "basepath", "EmptyFieldRule", "auto");
-df = readtable("Z:\home\ryanh\projects\ripple_heterogeneity\tmaze_sessions.csv", opts);
+% opts = delimitedTextImportOptions("NumVariables", 1);
+% opts.DataLines = [2, Inf];
+% opts.Delimiter = ",";
+% opts.VariableNames = "basepath";
+% opts.VariableTypes = "string";
+% opts.ExtraColumnsRule = "ignore";
+% opts.EmptyLineRule = "read";
+% opts = setvaropts(opts, "basepath", "WhitespaceRule", "preserve");
+% opts = setvaropts(opts, "basepath", "EmptyFieldRule", "auto");
+% df = readtable("Z:\home\ryanh\projects\ripple_heterogeneity\tmaze_sessions.csv", opts);
+
+df.basepath = {pwd};
 % visual of current edges (states) for linearized pos
 %    ___ ___
 %   | 2 | 1 |
@@ -35,7 +37,7 @@ end
 function run(basepath)
 
 basename = basenameFromBasepath(basepath);
-load(fullfile(basepath,[basename,'.animal.behavior.mat']))
+load(fullfile(basepath,[basename,'.animal.behavior.mat']),'behavior')
 
 if ~isfield(behavior,'states')
     return
@@ -45,25 +47,7 @@ states = states(~isnan(states));
 if length(states) == 3
     return
 end
-% drop states 2 and 4 down to the level of the other two arms (1&3) so
-% right and left will be contingous
-idx = behavior.states == 4 | behavior.states == 6 | behavior.states == 2;
 
-behavior.position.linearized(idx) = behavior.position.linearized(idx) -...
-    min(behavior.position.linearized(idx)) +...
-    max(behavior.position.linearized(behavior.states == 0));
-
-% relabel states
-states = behavior.states;
-idx = behavior.states == 4 | behavior.states == 6 | behavior.states == 2;
-states(idx) = 2;
-
-idx = behavior.states == 3 | behavior.states == 5 | behavior.states == 1;
-states(idx) = 1;
-behavior.states = states;
-
-% update statenames
-behavior.stateNames = {'middle','right','left'};
 
 % figure to verify states
 load(fullfile(basepath,[basename,'.session.mat']))
@@ -87,31 +71,78 @@ for ep = 1:length(session.epochs)
         end
     end
 end
-[idx,~,~] = InIntervals(behavior.timestamps,...
+[epoch_idx,~,~] = InIntervals(behavior.timestamps,...
     [start,stop]);
+epoch_idx = epoch_idx';
+
+
+figure;
+states = unique(behavior.states(epoch_idx));
+states = states(~isnan(states));
+subplot(1,2,1)
+for s = states
+    scatter(behavior.position.x(epoch_idx & behavior.states == s),...
+        behavior.position.y(epoch_idx & behavior.states == s),'DisplayName',num2str(s))
+    hold on
+end
+legend
+
+subplot(1,2,2)
+for s = states
+    scatter(behavior.timestamps(epoch_idx & behavior.states == s),...
+        behavior.position.linearized(epoch_idx & behavior.states == s),'DisplayName',num2str(s))
+    hold on
+end
+legend
+
+% drop states 2 and 4 down to the level of the other two arms (1&3) so
+% right and left will be contingous
+idx = epoch_idx & (behavior.states == 4 |...
+    behavior.states == 6 |...
+    behavior.states == 2);
+
+behavior.position.linearized(idx) = behavior.position.linearized(idx) -...
+    min(behavior.position.linearized(idx)) +...
+    max(behavior.position.linearized(epoch_idx & behavior.states == 0));
+
+% relabel states
+states = behavior.states;
+idx = epoch_idx & (behavior.states == 4 |...
+    behavior.states == 6 |...
+    behavior.states == 2);
+states(idx) = 2;
+
+idx = epoch_idx & (behavior.states == 3 |...
+    behavior.states == 5 |...
+    behavior.states == 1);
+states(idx) = 1;
+behavior.states(epoch_idx) = states(epoch_idx);
+
+% update statenames
+behavior.stateNames = {'middle','right','left'};
 
 colors = {[0 0.4470 0.7410],[0.8500 0.3250 0.0980],[0.9290 0.6940 0.1250]};
 figure;
 subplot(1,2,1)
-states = unique(behavior.states);
+states = unique(behavior.states(epoch_idx));
 states = states(~isnan(states));
 for s = states
-    scatter(behavior.position.x(idx' & behavior.states == s),...
-        behavior.position.y(idx' & behavior.states == s),[],colors{s+1})
+    scatter(behavior.position.x(epoch_idx & behavior.states == s),...
+        behavior.position.y(epoch_idx & behavior.states == s),[],colors{s+1})
     hold on
 end
 title('whole first epoch')
 
 subplot(1,2,2)
-states = unique(behavior.states);
+states = unique(behavior.states(epoch_idx));
 states = states(~isnan(states));
 for s = states
-    scatter(behavior.timestamps(idx' & behavior.states == s),...
-        behavior.position.linearized(idx' & behavior.states == s),[],colors{s+1})
+    scatter(behavior.timestamps(epoch_idx & behavior.states == s),...
+        behavior.position.linearized(epoch_idx & behavior.states == s),[],colors{s+1})
     hold on
 end
-xlim([start,start+60*5])
-title('first 5 minutes linear')
+% xlim([start,start+60*5])
+% title('first 5 minutes linear')
 
 
 prompt = "Does this look good? y/n: ";
