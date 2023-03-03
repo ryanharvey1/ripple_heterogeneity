195c4b18db6e680f3c9989fc76572a916fbf570f
Ryan Harvey <ryanh412@gmail.com>
2023-03-03 16:00:06 -0500

    speed up with pd.merge

--- ripple_heterogeneity/utils/add_new_deep_sup.py
+++ ripple_heterogeneity/utils/add_new_deep_sup.py
@@ -3,7 +3,7 @@ import numpy as np
 from ripple_heterogeneity.utils import loading
 
 
-def add_new_deep_sup_class(df, layer_dist=30):
+def add_new_deep_sup_class(df: pd.DataFrame, layer_dist: int = 30) -> pd.DataFrame:
     """
     Take df dataframe and update deepSuperficial classification
     Inputs:
@@ -13,49 +13,38 @@ def add_new_deep_sup_class(df, layer_dist=30):
         df: dataframe with deepSuperficial classification
     """
 
-    def assign_region(df, cell_metrics_):
-        """
-        Assign deepSuperficial classification based on distance from the pyramidal layer
-        """
-        basepath = df.basepath.iloc[0]
-        cell_metrics = cell_metrics_[cell_metrics_.basepath == basepath]
-
-        for uid in df.UID.unique():
-            deepSuperficialDistance = cell_metrics[cell_metrics.UID == uid].deepSuperficialDistance
-            df.loc[df.UID == uid, "deepSuperficialDistance"] = np.tile(deepSuperficialDistance, sum(df.UID == uid))
-
-            brainRegion = cell_metrics[cell_metrics.UID == uid].brainRegion
-            df.loc[df.UID == uid, "brainRegion"] = np.tile(brainRegion, sum(df.UID == uid))
-        return df
-
     cell_metrics = loading.load_all_cell_metrics(df.basepath.unique())
 
-    df_out = pd.DataFrame()
-    # iter over each unique basepath
-    for basepath in df.basepath.unique():
-        df_out = pd.concat(
-            [df_out, assign_region(df[df.basepath == basepath].copy(), cell_metrics)],
-            ignore_index=True,
-        )
+    df_out = df.merge(
+        cell_metrics[["basepath", "UID", "deepSuperficialDistance", "brainRegion"]],
+        on=["basepath", "UID"],
+        how="left",
+    )
 
     deep = -layer_dist
     middle = [-layer_dist, layer_dist]
     sup = layer_dist
     df_out.loc[df_out.deepSuperficialDistance <= deep, "deepSuperficial"] = "Deep"
+
     df_out.loc[
         (df_out.deepSuperficialDistance > middle[0])
         & (df_out.deepSuperficialDistance < middle[1]),
         "deepSuperficial",
     ] = "middle"
+
     df_out.loc[df_out.deepSuperficialDistance >= sup, "deepSuperficial"] = "Superficial"
-    
+
     # make sure these are ca1
-    df_out.loc[~df_out.brainRegion.str.contains("CA1|CA2"),"deepSuperficial"] = "unknown"
+    df_out.loc[
+        ~df_out.brainRegion.str.contains("CA1|CA2"), "deepSuperficial"
+    ] = "unknown"
 
     return df_out
 
 
-def deep_sup_from_deepSuperficialDistance(cell_metrics, layer_dist=30, hpc_assumption=True):
+def deep_sup_from_deepSuperficialDistance(
+    cell_metrics: pd.DataFrame, layer_dist: int = 30, hpc_assumption: bool = True
+) -> pd.DataFrame:
     """
     Assign deepSuperficial classification based on distance from the pyramidal layer
     Will work if you already have the (up to date) deepSuperficialDistance in the cell_metrics dataframe
@@ -69,20 +58,34 @@ def deep_sup_from_deepSuperficialDistance(cell_metrics, layer_dist=30, hpc_assum
     deep = -layer_dist
     middle = [-layer_dist, layer_dist]
     sup = layer_dist
-    cell_metrics.loc[cell_metrics.deepSuperficialDistance <= deep, "deepSuperficial"] = "Deep"
+
+    cell_metrics.loc[
+        cell_metrics.deepSuperficialDistance <= deep, "deepSuperficial"
+    ] = "Deep"
+
     cell_metrics.loc[
         (cell_metrics.deepSuperficialDistance > middle[0])
         & (cell_metrics.deepSuperficialDistance < middle[1]),
         "deepSuperficial",
     ] = "middle"
-    cell_metrics.loc[cell_metrics.deepSuperficialDistance >= sup, "deepSuperficial"] = "Superficial"
+
+    cell_metrics.loc[
+        cell_metrics.deepSuperficialDistance >= sup, "deepSuperficial"
+    ] = "Superficial"
 
     # make sure these are ca1
     if hpc_assumption:
-        cell_metrics.loc[~cell_metrics.brainRegion.str.contains("CA1|CA2"),"deepSuperficial"] = "unknown"
+        cell_metrics.loc[
+            ~cell_metrics.brainRegion.str.contains("CA1|CA2"), "deepSuperficial"
+        ] = "unknown"
 
     return cell_metrics
 
-def deep_sup_from_distance(cell_metrics, layer_dist=30, hpc_assumption=True):
+
+def deep_sup_from_distance(
+    cell_metrics: pd.DataFrame, layer_dist: int = 30, hpc_assumption: bool = True
+) -> pd.DataFrame:
     """calls deep_sup_from_deepSuperficialDistance"""
-    return deep_sup_from_deepSuperficialDistance(cell_metrics, layer_dist=layer_dist, hpc_assumption=hpc_assumption)
+    return deep_sup_from_deepSuperficialDistance(
+        cell_metrics, layer_dist=layer_dist, hpc_assumption=hpc_assumption
+    )
